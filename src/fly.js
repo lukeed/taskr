1999cca820b106b42fed926cbeea258beaa91e02
Jorge Bucaran <jbucaran@gmail.com>
2015-07-02 21:06:21 +0900

    remove this.reset &&  watch for link and add fs events

--- src/fly.js
+++ src/fly.js
@@ -9,49 +9,40 @@ import * as _ from "./util"
 
 export default class Fly extends Emitter {
   /**
+    Create a new instance of Fly. Use fly.start(...) to run tasks.
     @param {Object} host Flyfile
     @param {String} root Relative base path / root.
     @param {Array} plugins List of plugins to load.
-    @desc Create a new instance of Fly. Use fly.start(...) to run tasks.
    */
   constructor ({ host, root = "./", plugins = [] }) {
     super()
+    this.defer = _.defer
     this.encoding = process.env.ENCODING
     this.host = this.tasks = {}
-    this.defer = _.defer
     Object.keys(host).forEach((task) =>
       this.tasks[task] = this.host[task] = host[task].bind(this))
     process.chdir(this.root = root)
     plugins.forEach((plugin) => plugin.call(this))
   }
   /**
-    @desc Log a message with a time stamp as defined in fly-fmt.
+    Log a message with a time stamp as defined in /fmt.
    */
   log (...args) {
     _.log(`[${fmt.time}] ${args}`)
     return this
   }
   /**
-    @desc Reset the internal state.
-   */
-  reset () {
-    this._src = []
-    this._filters = []
-    this._writers = []
-    return this
-  }
-  /**
+    Clear each specified directory. Wraps rimraf.
     @param {...String} paths
-    @desc Clear each specified directory. Wraps rimraf.
    */
   clear (...paths) {
     const _clear = this.defer(rimraf)
     return paths.map((path) => _clear(path))
   }
   /**
+    Concatenates files read via source.
     @param {String} name File name to concatenate unwrapped sources.
-    @desc Concatenates files read via source.
-    @todo: clear files before appending to them.
+    @todo: Append only to files that do not exist.
    */
   concat (name) {
     this._writers.push(({ dest, data }) => {
@@ -61,25 +52,27 @@ export default class Fly extends Emitter {
     return this
   }
   /**
+    Add a filter to be applied when unwrapping the source promises.
     @param {...Function} filters
-    @desc Add a filter to be applied when unwrapping the source promises.
    */
   filter (...filters) {
     filters.forEach((filter) => this._filters.push(filter))
     return this
   }
   /**
+    Run the specified tasks when a change is detected in the globs.
     @param {Array} globs Glob pattern to watch for changes.
     @param {...String} tasks List of tasks to apply.
-    @desc Run the specified tasks when a change is detected in the globs.
    */
   watch (globs, ...tasks) {
-    _.watch(globs, ...tasks).on("change", () => this.start(tasks))
+    ["change", "add", "unlink"].forEach((event) => {
+      _.watch(globs).on(event, () => this.start(tasks))
+    })
     return this
   }
   /**
+    Runs the specified tasks.
     @param {Array} tasks List of tasks to run
-    @desc Runs the specified tasks.
    */
   start (tasks = []) {
     if (tasks.length === 0) tasks.push("default")
@@ -102,24 +95,28 @@ export default class Fly extends Emitter {
     })
   }
   /**
+    Creates an array of promises with read sources from a list of globs.
+    When a promise resolved, the data source is reduced applying each of
+    the existing filters.
     @param {...String} globs Glob pattern
-    @desc expand resolves to an array of file names from the glob pattern.
-    Each file is mapped to a read file promise that resolves in a recursive
-    filter returning { file, data }
+    @return Fly instance. Promises resolve to { file, data }
   */
   source (...globs) {
+    this._source = []
+    this._filters = []
+    this._writers = []
+
     globs.forEach((pattern) => {
       const base = ((base) => {
-        return base.length > 1
-          ? base.shift()
-          : ""
+        return base.length > 1 ? base.shift() : ""
       }(pattern.split("*")))
-      this.reset()._src.push(_.expand(pattern, (files) => {
+
+      this._source.push(_.expand(pattern, (files) => {
         return files.map((file) => {
           return fs.readFile(file, this.encoding)
-            .then((data) => (function filter (data, filters) {
+            .then((data) => (function reduce (data, filters) {
               return (filters.length > 0)
-                ? filter(filters[0].call(this, data), filters.slice(1))
+                ? reduce(filters[0].call(this, data), filters.slice(1))
                 : { file, data, base }
             }.call(this, `${data}`, this._filters)))
         })
@@ -128,21 +125,22 @@ export default class Fly extends Emitter {
     return this
   }
   /**
+    Resolves an array of promises an return a new promise with the result.
+    By default resolves the current promise sources.
     @param {Array:Promise} source
-    @desc Resolves an array of promises an return a new promise with the result.
    */
-  unwrap (source = this._src) {
+  unwrap (source = this._source) {
     return new Promise((resolve) => {
       Promise.all(source).then((result) => resolve.apply(this, result))
     })
   }
   /**
+    Resolves all source promises and writes to each destination path.
     @param {...String} dest Destination paths
-    @desc Resolves all source promises and writes to each destination path.
    */
   target (...dest) {
     return Promise.all(dest.map((dest) => {
-      return this.unwrap(this._src).then((files) => {
+      return this.unwrap(this._source).then((files) => {
         return files.map(({ file, data, base }) => {
           return ((file) => {
             if (!this._writers.length) {
