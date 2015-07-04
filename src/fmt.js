2031994107af54094318887e6fcc8eca4f4e4929
Jorge Bucaran <jbucaran@gmail.com>
2015-07-04 18:02:41 +0900

    add title to fmt

--- src/fmt.js
+++ src/fmt.js
@@ -5,6 +5,7 @@ import date from "dateformat"
 export default Object.assign(clor, {
   complete: clor.blue.bold("\"%s\""),
   start: clor.bold.yellow("\"%s\""),
+  title: clor.bold.yellow("%s"),
   error: clor.bold.red("%s"),
   path: clor.underline.cyan("%s"),
   warn: clor.bold.magenta("%s"),
