86cc0bc529b23da98095135a40a311dc9832c631
Jorge Bucaran <jbucaran@me.com>
2015-07-05 00:37:41 +0900

    rebuild :package:

--- dist/cli/spawn.js
+++ dist/cli/spawn.js
@@ -21,12 +21,13 @@ var _path2 = _interopRequireDefault(_path);
 /**
   Resolve flyfile using flypath and create a new Fly instance.
   @param {String} flypath Path to a flyfile
-  */
+ */
 exports["default"] = _regeneratorRuntime.mark(function callee$0$0(flypath) {
-  var root, load, pkg;
+  var host, root, load, pkg, plugins;
   return _regeneratorRuntime.wrap(function callee$0$0$(context$1$0) {
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
+        host = require(flypath);
         root = _path2["default"].dirname(flypath);
 
         load = function load() {
@@ -37,21 +38,18 @@ exports["default"] = _regeneratorRuntime.mark(function callee$0$0(flypath) {
           return require(_path2["default"].join.apply(_path2["default"], [root].concat(file)));
         };
 
-        pkg = (function (pkg) {
+        pkg = (function () {
           try {
-            return load(pkg);
+            return load("package");
           } catch (_) {}
-        })("package");
+        })();
 
-        return context$1$0.abrupt("return", new _fly2["default"]({
-          root: root,
-          host: require(flypath),
-          plugins: (0, _util.plugins)({ pkg: pkg }).reduce(function (prev, next) {
-            return prev.concat(load("node_modules", next));
-          }, [])
-        }));
+        plugins = (0, _util.searchPlugins)({ pkg: pkg }).reduce(function (prev, next) {
+          return prev.concat(load("node_modules", next));
+        }, []);
+        return context$1$0.abrupt("return", new _fly2["default"]({ host: host, root: root, plugins: plugins }));
 
-      case 4:
+      case 6:
       case "end":
         return context$1$0.stop();
     }
