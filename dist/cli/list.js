f05013a1835c7f0c2d9bf8948b782955ecf7ca81
Jorge Bucaran <jbucaran@gmail.com>
2015-07-04 18:01:37 +0900

    rebuild :package:

--- dist/cli/list.js
+++ dist/cli/list.js
@@ -27,7 +27,8 @@ exports["default"] = function (flyfile, _ref) {
   if (!simple) (0, _util.log)("\n" + _fmt2["default"].dim.bold("Available tasks"));
   _Object$keys(host).forEach(function (key) {
     var match = /^\s*\/\*\*\s*@desc\s+(.*)\s*\*\//gm.exec("" + host[key]);
-    (0, _util.log)((simple ? "%s" : _fmt2["default"].title + "\t") + " " + (match ? match[0] : ""), key);
+    var description = match ? match.pop() : "";
+    (0, _util.log)("" + (simple ? "%s" : "  " + _fmt2["default"].title + "\t" + description), key);
   });
   if (!simple) (0, _util.log)();
 };
