3bf727d05d811a2d8645a0bffe7d52b2d28075c4
Jorge Bucaran <jbucaran@me.com>
2015-07-06 06:59:34 +0900

    remove co and let the CLI engine handle it

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,13 @@
 #!/bin/sh
 ":" //# comment; exec /usr/bin/env node --harmony --harmony_arrow_functions "$0" "$@"
 
-if (!require("yieldables"))
-  throw new Error("Fly requires node >= 0.11")
+if (!require("yieldables")) throw new Error("Fly requires node >= 0.11")
 
-require("co")(require("../dist")).catch(function (e) {
+require("../dist").catch(function (e) {
   if (e.code === "ENOENT") {
     console.error(
-      "Your project is missing a Flyfile. For more info about" +
-      "\nrunning Fly, see the Fly Start guide: git.io/fly-start")
+      "Your project is missing a Flyfile. For more info about\n" +
+      "running Fly, see the Fly Start guide: git.io/fly-start")
   } else {
     require("../dist/util").trace(e)
   }
