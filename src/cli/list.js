3de4498542ae95681a0d73bfcfc7c8bcf4bf297c
Jorge Bucaran <jbucaran@gmail.com>
2015-07-05 16:16:05 +0900

    support for es6/7 module default export as the 'default' task

--- src/cli/list.js
+++ src/cli/list.js
@@ -5,14 +5,22 @@ import { log } from "../util"
   List all tasks available in the flyfile
   @param {Object} flyfile
   @param {Object} opts.simple Simple task listing.
-  */
+ */
 export default function (flyfile, { simple }) {
   const host = require(flyfile)
   if (!simple) log(`\n${fmt.dim.bold("Available tasks")}`)
-  Object.keys(host).forEach((key) => {
-    const match = /^\s*\/\*\*\s*@desc\s+(.*)\s*\*\//gm.exec(`${host[key]}`)
+
+  each(host instanceof Function
+    ? Object.assign(host, { default: host })
+    : host, (key, value) => {
+    const match = /^\s*\/\*\*\s*@desc\s+(.*)\s*\*\//gm.exec(`${value}`)
     const description = match ? match.pop() : ""
     log(`${simple ? "%s" : "  " + fmt.title + "\t" + description}`, key)
   })
+
   if (!simple) log()
+}
+
+function each (host, cb) {
+  Object.keys(host).forEach((key) => cb(key, host[key]))
 }
