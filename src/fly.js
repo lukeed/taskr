676414881ccd118822d82d30ba2bd7bb971dd051
Jorge Bucaran <jbucaran@gmail.com>
2015-07-11 15:00:56 +0900

    0.2.0 mega update
    + support for async transformers
    + add Fly.prototype.writer method to append writers / IO hooks (concat, etc)
    + add option to run the promise sequence in parallel to each expanded set of globs

--- src/fly.js
+++ src/fly.js
@@ -2,10 +2,10 @@ import co from "co"
 import fs from "mz/fs"
 import mkdirp from "mkdirp"
 import rimraf from "rimraf"
-import { dirname, join } from "path"
+import { dirname, join, parse } from "path"
 import Emitter from "./emitter"
 import fmt from "./fmt"
-import * as _ from "./util"
+import * as _ from "fly-util"
 
 export default class Fly extends Emitter {
   /**
@@ -17,7 +17,7 @@ export default class Fly extends Emitter {
   constructor ({ host, root = "./", plugins = [] }) {
     super()
     this.defer = _.defer
-    this.encoding = process.env.ENCODING
+    this.encoding = process.env.ENCODING || "utf8"
     this.host = this.tasks = host instanceof Function
       ? Object.assign(host, { default: host }) : host
 
@@ -28,14 +28,14 @@ export default class Fly extends Emitter {
     plugins.forEach((plugin) => plugin.call(this))
   }
   /**
-   * Log a message with a time stamp as defined in /fmt.
+   * Log a message with a time stamp.
    */
   log (...args) {
     _.log(`[${fmt.time}] ${args}`)
     return this
   }
   /**
-   * Clear each specified directory. Wraps rimraf.
+   * Rimraf paths.
    * @param {...String} paths
    */
   clear (...paths) {
@@ -48,21 +48,31 @@ export default class Fly extends Emitter {
    * @TODO: by default this operation should clear the target file to concat
    */
   concat (name) {
-    this._writers.push(({ dest, data }) => {
-      mkdirp.sync(dest)
-      fs.appendFile(join(dest, name), data, this.encoding)
+    this.write(function* ({ path, source }) {
+      mkdirp.sync(path)
+      yield fs.appendFile(join(path, name), source, this.encoding)
     })
     return this
   }
   /**
-   * Add a filter to be applied when unwrapping the source promises.
+   * Append a writer to be applied after the globs are expanded and sources
+   * read. Examples of writers are the default IO writer and concat.
+   * @param {Generator} function yielding a promise.
+   */
+  write (writer) {
+    this._writers.push(writer.bind(this))
+    return this
+  }
+  /**
+   * Add a transformer filter. If specified by name and Fly.prototype[name]
+   * is undefined, extends prototype with a method closing on filter.
    * @param
    *   {String} name of the filter.
    *   {Object} { filter, options } object.
    *   {Function} filter function.
    * @param [{Function}] filter function.
    */
-  filter (name, filter) {
+  filter (name, filter, ext = "") {
     if (name instanceof Function) {
       this.filter({ filter: name })
     } else if (typeof name === "object") {
@@ -70,12 +80,12 @@ export default class Fly extends Emitter {
     } else {
       if (this[name] instanceof Function)
         throw new Error(`${name} method already defined in instance.`)
-      this[name] = (options) => this.filter({ filter, options })
+      this[name] = (options) => this.filter({ filter, options, ext })
     }
     return this
   }
   /**
-   * Watch for changes on globs and run each of the specified tasks.
+   * Watch for IO events in globs and run tasks.
    * @param {Array:String} glob patterns to observe for changes
    * @param {Array:String} list of tasks to run on changes
    */
@@ -85,17 +95,14 @@ export default class Fly extends Emitter {
         .on("all", () => this.start(tasks)))
     return this
   }
-
   /**
-   * Runs the specified tasks. This method can be yielded inside a task.
-   * The return value of each task (after the first) is passed on to the
-   * next task in the series.
+   * Run tasks. Each task's return value cascades on to the next task in
+   * a series. Can be yielded inside a task.
    * @param {Array} list of tasks to run
    * @return {Promise}
    */
   start (tasks = []) {
-    /** @deprecated default tasks will revert to `default`in 0.2.0 */
-    if (tasks.length === 0) tasks.push(this.host.default ? "default" : "main")
+    if (tasks.length === 0) tasks.push("default")
     return co.call(this, function* (ret) {
       for (const task of []
         .concat(tasks)
@@ -115,64 +122,71 @@ export default class Fly extends Emitter {
     })
   }
   /**
-   * Creates an array of promises with read sources from a list of globs.
-   * When a promise resolved, the data source is reduced applying each of
-   * the existing filters.
+   * Compose a yieldable sequence. Initialize globs, filters and writers.
    * @param {...String} glob patterns
-   * @return Fly instance. Promises resolve to { file, data }
+   * @return Fly instance. Promises resolve to { file, source }
    */
-  source (...globs) {
-    this._source = []
-    this._filters = []
-    this._writers = []
-
-    _.flatten(globs).forEach((pattern) => {
-      const base = ((base) => {
-        return base.length > 1 ? base.shift() : ""
-      }(pattern.split("*")))
-
-      this._source.push(_.expand(pattern, (files) => {
-        return files.map((file) => {
-          return fs.readFile(file, this.encoding)
-            .then((data) => (function reduce (data, filters) {
-              return (filters.length > 0)
-                ? reduce(filters[0].filter
-                  .apply(this, [data, filters[0].options]), filters.slice(1))
-                : { file, data, base }
-            }.call(this, `${data}`, this._filters)))
-        })
-      }))
-    })
-    return this
-  }
+   source (...globs) {
+     this._globs = _.flatten(globs)
+     this._filters = []
+     this._writers = []
+     return this
+   }
+   /**
+    * Unwrap the source globs.
+    * @param {Function} onFulfilled
+    * @param {onRejected} onFulfilled
+    */
+    unwrap (onFulfilled, onRejected) {
+      return new Promise((resolve, reject) => {
+        Promise.all(this._globs.map(glob => _.expand(glob)))
+          .then((result) => resolve.apply(this, result)).catch(reject)
+      }).then(onFulfilled).catch(onRejected)
+    }
   /**
-   * Resolve an array of promises an return a new promise with the result.
-   * By default resolves the current promise sources.
-   * @param {Array:Promise} source
+   * Yield expanded glob promises and filter-reduce sources.
+   * @param {Array} destination paths
+   * @param {Boolean} apply filters to each glob pattern in parallel
    */
-  unwrap (source = this._source) {
-    return new Promise((resolve) => {
-      Promise.all(source).then((result) => resolve.apply(this, result))
-    })
-  }
-  /**
-   * Resolves all source promises and writes to each destination path.
-   * @param {...String} destination paths
-   */
-  target (...dest) {
-    return Promise.all(_.flatten(dest).map((dest) => {
-      return this.unwrap(this._source).then((files) => {
-        return files.map(({ file, data, base }) => {
-          return ((file) => {
-            if (!this._writers.length) {
-              mkdirp.sync(dirname(file))
-              return fs.writeFile(file, data, this.encoding)
-            }
-            return this._writers.map((write) =>
-              write({ dest, base, file, data }))
-            }( join(dest, file.replace(base, "")) ))
-        })
-      })
-    }))
-  }
+   *target (dest, { parallel = false } = {}) {
+     const go = (glob, dest) => {
+       const base = (_ => _.length ? "" : _.shift())(glob.split("*"))
+       return function* () {
+         for (const file of yield _.expand(glob)) {
+           yield function* reduce (source, filters, ext = "") {
+             if (filters.length === 0) {
+               for (const path of dest) {
+                 const target = (file =>
+                   join(path, `${file.name}${ext ? ext : file.ext}`)
+                 )(parse(file))
+                 if (!this._writers.length) {
+                   this.write(function* ({ target, source }) {
+                      mkdirp.sync(dirname(target)) // TODO cache?
+                      yield fs.writeFile(target, source, this.encoding)
+                   })
+                 }
+                 yield this._writers.map((write) =>
+                   write({ path, target, base, file, source }))
+               }
+             } else {
+               const _ = filters[0]
+               yield function* (filter, ext) {
+                 yield reduce.call(this, filter instanceof Promise
+                   ? yield filter
+                   : filter, filters.slice(1), ext)
+               }.call(this, _.filter.apply(this,
+                 [source, _.options]), _.ext)
+             }
+           }.call(this, `${yield fs.readFile(file)}`, this._filters)
+         }
+       }.call(this)
+     }
+     if (parallel) {
+         yield this._globs.map((glob) => go(glob, _.flatten(dest)))
+     } else {
+       for (const glob of this._globs) {
+         yield go(glob, _.flatten(Array.isArray(dest) ? dest : [dest]))
+       }
+     }
+   }
 }
