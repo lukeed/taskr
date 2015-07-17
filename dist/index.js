5cb9db362daa7df713933dc7a6e77f1b529afff4
Jorge Bucaran <jbucaran@me.com>
2015-07-17 17:16:37 +0900

    build :package:

--- dist/index.js
+++ dist/index.js
@@ -34,67 +34,68 @@ var _package2 = _interopRequireDefault(_package);
 
 (0, _flyUtil.notifyUpdates)({ pkg: _package2["default"] });
 
-var _Parsec$parse$options$options$options$options = _parsec2["default"].parse(process.argv).options("file", { "default": "./" }).options("list").options("help").options("version");
-
-var help = _Parsec$parse$options$options$options$options.help;
-var list = _Parsec$parse$options$options$options$options.list;
-var file = _Parsec$parse$options$options$options$options.file;
-var version = _Parsec$parse$options$options$options$options.version;
-var tasks = _Parsec$parse$options$options$options$options._;
 exports["default"] = _regeneratorRuntime.mark(function callee$0$0() {
-  var path;
+  var _Parsec$parse$options$options$options$options, help, list, file, version, tasks, path;
+
   return _regeneratorRuntime.wrap(function callee$0$0$(context$1$0) {
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
+        _Parsec$parse$options$options$options$options = _parsec2["default"].parse(process.argv).options("file", { "default": "./" }).options("list").options("help").options("version");
+        help = _Parsec$parse$options$options$options$options.help;
+        list = _Parsec$parse$options$options$options$options.list;
+        file = _Parsec$parse$options$options$options$options.file;
+        version = _Parsec$parse$options$options$options$options.version;
+        tasks = _Parsec$parse$options$options$options$options._;
+
         if (!help) {
-          context$1$0.next = 4;
+          context$1$0.next = 10;
           break;
         }
 
         cli.help();
-        context$1$0.next = 24;
+        context$1$0.next = 30;
         break;
 
-      case 4:
+      case 10:
         if (!version) {
-          context$1$0.next = 8;
+          context$1$0.next = 14;
           break;
         }
 
         cli.version(_package2["default"]);
-        context$1$0.next = 24;
+        context$1$0.next = 30;
         break;
 
-      case 8:
-        context$1$0.next = 10;
+      case 14:
+        context$1$0.next = 16;
         return (0, _flyUtil.findPath)(file);
 
-      case 10:
+      case 16:
         path = context$1$0.sent;
 
         if (!list) {
-          context$1$0.next = 15;
+          context$1$0.next = 21;
           break;
         }
 
-        cli.list(path, { simple: list === "simple" });
-        context$1$0.next = 24;
+        cli.list(path, { simple: list === "bare" });
+        context$1$0.next = 30;
         break;
 
-      case 15:
+      case 21:
         context$1$0.t0 = _reporter2["default"];
         context$1$0.t1 = _fly2["default"];
-        context$1$0.next = 19;
+        context$1$0.next = 25;
         return cli.spawn(path);
 
-      case 19:
+      case 25:
         context$1$0.t2 = context$1$0.sent;
         context$1$0.t3 = new context$1$0.t1(context$1$0.t2);
         context$1$0.t4 = { path: path };
         context$1$0.t5 = tasks;
         return context$1$0.abrupt("return", context$1$0.t0.call.call(context$1$0.t0, context$1$0.t3).emit("fly_run", context$1$0.t4).start(context$1$0.t5));
 
-      case 24:
+      case 30:
       case "end":
         return context$1$0.stop();
     }
