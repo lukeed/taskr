4b229eacad12b48f4ff7b1e8eb8a3d282d371a68
Jorge Bucaran <jbucaran@gmail.com>
2015-08-03 19:15:33 +0900

    add error handling and co

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-import Fly from "./fly"
+import co from "co"
 import Parsec from "parsec"
 import reporter from "./reporter"
 import * as cli from "./cli/"
-import { notifyUpdates, findPath as find } from "fly-util"
+import { notifyUpdates, error, trace } from "fly-util"
 import pkg from "../package"
 
-notifyUpdates({ pkg })
-
-export default function* () {
-  let { help, list, file, version, _: tasks } =
-    Parsec.parse(process.argv)
-      .options("file", { default: "./" })
-      .options("list")
-      .options("help")
-      .options("version")
+co(function* () {
+  notifyUpdates({ pkg })
+  const { help, list, file, version, _: tasks } = Parsec
+    .options("file")
+    .options("list")
+    .options("help")
+    .options("version")
+    .parse(process.argv, { strictMode: true })
 
   if (help) {
     cli.help()
   } else if (version) {
     cli.version(pkg)
   } else {
-    const path = yield find(file)
+    const fly = yield cli.spawn(file)
     if (list) {
-      cli.list(path, { simple: list === "bare" })
+      cli.list(fly.host, { bare: list === "bare" })
     } else {
-     return reporter
-      .call(new Fly(yield cli.spawn(path)))
-      .emit("fly_run", { path })
-      .start(tasks)
+      return reporter
+        .call(fly)
+        .emit("fly_run", { path: fly.file })
+        .start(tasks)
     }
   }
-}
+}).catch((e) => {
+  if (e.code === "ENOENT")
+    error(`No Flyfile? See the Quickstart guide → git.io/fly-quick`)
+  else if (e.code === "INVALID_OPTION")
+    error(`Unknown Flag: -${e.key}. Run fly -h to see the options.`)
+  else trace(e)
+})
