ec7f33f210e8475f1b6064732bd3ca9b6e7e22d0
Jorge Bucaran <jbucaran@gmail.com>
2015-08-16 02:23:42 +0900

    fix build

--- flyfile.js
+++ flyfile.js
@@ -4,9 +4,10 @@ const paths = {
 }
 
 export default function* () {
+  yield this.clear(paths.dist)
   yield this
     .source(paths.src).eslint()
-  yield this
+  yield this.log("Building Fly...")
     .source(paths.src)
     .babel({ optional: ["runtime"], modules: "common" })
     .target(paths.dist)
