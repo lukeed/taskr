ab07576e16a7b4023d31643ca5d8c3194b784b90
Jorge Bucaran <jbucaran@gmail.com>
2015-08-16 17:05:17 +0900

    rewrite async example in es7

--- examples/async/flyfile.js
+++ examples/async/flyfile.js
@@ -1,14 +1,13 @@
-export default function* () {
-  yield this.clear("dist")
-  yield this
+export default async function () {
+  const transform = (source, options, cb) => {
+    setTimeout(() => {
+      cb(null, source.split("").reverse().join(""))
+    }, options.time)
+  }
+  await this.clear("dist")
+  await this
     .source("src/*.txt")
     .filter(s => this.defer(transform)(s, { time: 1000 }))
     .filter(s => s.toUpperCase())
     .target("dist")
-}
-
-function transform (source, options, cb) {
-  setTimeout(() => {
-    cb(null, source.split("").reverse().join(""))
-  }, options.time)
 }
