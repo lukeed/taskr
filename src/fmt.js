798792278b44d50b80ca1af0ace11688f7568765
Jorge Bucaran <jbucaran@gmail.com>
2015-08-19 22:10:55 +0900

    clor must not override Function.name

--- src/fmt.js
+++ src/fmt.js
@@ -1,12 +1,11 @@
 import clor from "clor"
 
 export default Object.assign(clor, {
-  complete: clor.blue.bold("\"%s\""),
-  start: clor.bold.yellow("\"%s\""),
-  title: clor.bold.yellow("%s"),
+  complete: clor.blue.bold(`"%s"`),
+  start: clor.bold.yellow(`"%s"`),
+  title: clor.bold.yellow(`"%s"`),
   error: clor.bold.red("%s"),
   path: clor.underline.cyan("%s"),
   warn: clor.bold.magenta("%s"),
-  name: clor.bold.yellow("\"%s\""),
   secs: clor.green("%d %s")
 })
