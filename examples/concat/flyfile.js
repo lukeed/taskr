dfe63f78e174c7fd36063ac39d4aac2c7ff78126
Marvin Roger <nivramdu94@gmail.com>
2015-11-26 23:49:07 +0100

    Upgrade to Babel 6

--- examples/concat/flyfile.js
+++ examples/concat/flyfile.js
@@ -2,7 +2,7 @@ export default function* () {
   yield this.clear("dist")
   yield this
     .source("src/*.js")
-    .babel({ stage: 0, sourceMap: true })
+    .babel({ presets: ["es2015"], sourceMap: true })
     // .uglify({})
     .concat("out.js")
     .target("dist")
