c312865bd05b3005add77468eb7345ac51f0b67c
luke <luke.edwards05@gmail.com>
2016-02-21 15:41:28 -0800

    rewrite emitter class, should use node events?

--- src/emitter.js
+++ src/emitter.js
@@ -1,21 +1,28 @@
-export default class Emitter {
-  constructor () { this.events = [] }
-  /**
-   * Observe an event.
-   * @param {String} name of event to observe
-   * @param {Function} handler
-   */
-  on (name, cb) {
-    (this.events[name] = this.events[name] || []).push(cb)
-    return this
-  }
-  /**
-   * Emit an event to observers.
-   * @param {String} name of event to emit
-   * @param {Object} data to send
-   */
-  emit (name, obj) {
-    (this.events[name] || []).forEach((event) => event.call(this, obj))
-    return this
-  }
+'use strict';
+
+function Emitter () {
+	this.events = [];
 }
+
+/**
+ * Observe an event
+ * @param  {String}   name 	The name of the event to observe
+ * @param  {Function} cb   	handler
+ * @return {Emitter}
+ */
+Emitter.prototype.on = function(name, cb) {
+	(this.events[name] = this.events[name] || []).push(cb);
+  return this;
+};
+
+/**
+ * Emit an event to observers.
+ * @param {String} name 	The name of the event to emit.
+ * @param {Object} obj 		The data to send
+ */
+Emitter.prototype.emit = function(name, obj) {
+  (this.events[name] || []).forEach((event) => event.call(this, obj));
+  return this;
+};
+
+module.exports = Emitter;
