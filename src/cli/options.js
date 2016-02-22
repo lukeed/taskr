c5edc2603764a26ba05b5e39048aa5003d2b0b1a
luke <luke.edwards05@gmail.com>
2016-02-21 23:05:19 -0800

    rewrite cli.options

--- src/cli/options.js
+++ src/cli/options.js
@@ -1,9 +1,14 @@
-import parse from "parsec"
-export function options () {
-  return parse(
-    ["f", "file", { default: "." }],
-    "list",
-    "help",
-    "version",
-    ["_", "tasks"], (key) => { throw { code: "UNKNOWN_OPTION", key } })
-}
+var parse = require('parsec');
+
+module.exports = function () {
+	return parse(
+		['f', 'file', {default: '.'}],
+		'list',
+		'help',
+		'version',
+		['_', 'tasks'],
+		function (key) {
+			throw new Error({code: 'UNKNOWN_OPTION', key: key});
+		}
+	);
+};
