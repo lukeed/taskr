0681b1544f2b1ac2f400bc56f5b105373f6a2639
luke <luke.edwards05@gmail.com>
2016-02-22 00:23:38 -0800

    rewrite / remove utils.filter

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,22 +1,28 @@
-import debug from "debug"
-import { flatten } from "./flatten"
-const _ = debug("fly:filter")
+'use strict';
+
+var flatten = require('flatten');
+var debug = require('debug');
+var _ = debug('fly:filter');
 
 /**
-  Filter fly-* plugins from a package dependencies.
-  @param {Object} package.json
-  @param {Function} load handler
-  @param {[String]} blacklisted plugins
-  @return {[String]} list of fly plugins
+	Filter fly-* plugins from a package dependencies.
+	@param {Object} package.json
+	@param {Function} load handler
+	@param {[String]} blacklisted plugins
+	@return {[String]} list of fly plugins
 */
 
-export function filter(pkg, load, blacklist = []) {
-  _("filter fly-* plugins")
-  return !pkg ? []
-    : flatten(["dependencies", "devDependencies", "peerDependencies"]
-      .filter((key) => key in pkg)
-      .map((dep) => Object.keys(pkg[dep])))
-      .filter((dep) => /^fly-[-\w]+/g.test(dep))
-      .filter((dep) => !~["fly-util"].concat(blacklist).indexOf(dep))
-      .reduce((prev, next) => prev.concat(load(next)), [])
-}
+// @todo: use `pkg-dir` & `read-pkg` libs
+module.exports = function (pkg, load, blacklist) {
+	blacklist = blacklist || [];
+
+	_('filter fly-* plugins');
+
+	// return !pkg ? []
+	// 	: flatten(['dependencies', 'devDependencies', 'peerDependencies']
+	// 		.filter((key) => key in pkg)
+	// 		.map((dep) => Object.keys(pkg[dep])))
+	// 		.filter((dep) => /^fly-[-\w]+/g.test(dep))
+	// 		.filter((dep) => !~['fly-util'].concat(blacklist).indexOf(dep))
+	// 		.reduce((prev, next) => prev.concat(load(next)), []);
+};
