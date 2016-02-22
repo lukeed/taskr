8eacdf6460719ed6c06ddd9f13a39fb899a45ac7
luke <luke.edwards05@gmail.com>
2016-02-22 00:30:48 -0800

    rewrite base flyfile

--- flyfile.js
+++ flyfile.js
@@ -1,17 +1,16 @@
-const paths = {
-  src: "src/**/*.js",
-  dist: "dist"
-}
+var x = module.exports;
+var paths = {
+	src: 'src/**/*.js',
+	dist: 'dist'
+};
 
-export default function* () {
-  yield this
-    .source(paths.src).eslint()
-
-  yield this.clear(paths.dist)
-  yield this.log("Building Fly...")
-    .source(paths.src)
-    .babel({
-      presets: ["es2015", "stage-0"]
-    })
-    .target(paths.dist)
-}
+x.default = function * () {
+	yield this.source(paths.src).xo();
+	yield this.clear(paths.dist);
+	yield this.log('Building Fly...')
+		.source(paths.src)
+		.babel({
+			presets: ['es2015', 'stage-0']
+		})
+		.target(paths.dist);
+};
