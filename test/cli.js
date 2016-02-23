f31d3ba49774ab02a223ae172e86fb2fb154037e
lukee <luke.edwards05@gmail.com>
2016-02-23 14:00:09 -0800

    rewrite cli tests (incomplete)

--- test/cli.js
+++ test/cli.js
@@ -1,79 +1,97 @@
-import co from "co"
-import test from "tape"
-import { test as tlog } from "./helpers/testLog"
-import Fly from "../src/fly"
-import * as cli from "../src/cli"
-import pkg from "../package"
-import { join } from "path"
+'use strict';
 
-test("✈  cli", (t) => {
-  t.ok(cli !== undefined, "is defined")
-  Array.prototype.concat(["help", "list", "spawn", "version"])
-  .forEach((command) =>
-    t.equal(command in cli, true, `${command} is defined`))
-  t.end()
-})
+var co = require('co');
+var test = require('tape').test;
+var tlog = require('./helpers/testLog');
+var Fly = require('../lib/fly');
+var cli = require('../lib/cli');
+var pkg = require('../package');
+var join = require('path').join;
 
-test("✈  cli.version", (t) => {
-  const flypath = join(process.cwd(), "test", "fixtures", "flyfile.js")
-  tlog.call(t, () => cli.version(pkg), (actual) => {
-    t.equal(actual, `${pkg.name}, ${pkg.version}`, "log fly version")
-  })
-  t.end()
-})
+var flypath = join(process.cwd(), 'test', 'fixtures', 'flyfile.js');
 
-test("✈  cli.help", (t) => {
-  const flypath = join(process.cwd(), "test", "fixtures", "flyfile.js")
-  tlog.call(t, cli.help, (actual) => {
-    t.equal(actual, "\n\x1b[2m\x1b[1mUsage\x1b[0m\n  fly [options] [tasks]\n\n\x1b[2m\x1b[1mOptions\x1b[0m\n  \x1b[2m\x1b[1m-\x1b[0m\x1b[1mh \x1b[0m\x1b[2m\x1b[1m --\x1b[0m\x1b[1mhelp\x1b[0m     Display this help.\n  \x1b[2m\x1b[1m-\x1b[0m\x1b[1mf \x1b[0m\x1b[2m\x1b[1m --\x1b[0m\x1b[1mfile\x1b[0m     Use an alternate Flyfile.\n  \x1b[2m\x1b[1m-\x1b[0m\x1b[1ml \x1b[0m\x1b[2m\x1b[1m --\x1b[0m\x1b[1mlist\x1b[0m     Display available tasks.\n  \x1b[2m\x1b[1m-\x1b[0m\x1b[1mv \x1b[0m\x1b[2m\x1b[1m --\x1b[0m\x1b[1mversion\x1b[0m  Display version.\n  ",
-      "show fly help")
-  })
-  t.end()
-})
+test('✈  cli', function (t) {
+	t.ok(cli !== undefined, 'is defined');
+	['help', 'list', 'spawn', 'version'].forEach(function (command) {
+		t.equal(command in cli, true, command + ' is defined');
+	});
+	t.end();
+});
 
-test("✈  cli.list", (t) => {
-  const message = "list tasks in a fly instance"
-  const flypath = join(process.cwd(), "test", "fixtures", "flyfile.js")
-  Array.prototype.concat([true, false]).forEach((bare) => {
-    tlog.call(t, () => cli.list(require(flypath), { bare: bare }),
-    (actual, key) => {
-      if (!bare && actual === '\n\x1b[2m\x1b[1mAvailable tasks\x1b[0m')
-        return true
-      switch (key) {
-        case "a":
-          return true
-        case "b":
-          return true
-        case "c":
-          t.ok(true, `message / bare ${bare}`)
-          break
-        default:
-          t.ok(false, message)
-      }
-    })
-  })
-  t.end()
-})
+test('✈  cli.version', function (t) {
+	tlog.call(t, function () {
+		cli.version(pkg);
+	}, function (actual) {
+		t.equal(actual, pkg.name + ', ' + pkg.version, 'log fly version');
+	});
+	t.end();
+});
 
-test("✈  cli.spawn", (t) => {
-  t.plan(6)
-  Array.prototype.concat([
-    join("test", "fixtures", "alt"),
-    join("test", "fixtures", "alt", "flyfile.js")
-  ]).map((flypath) => ({
-    spawn: co.wrap(cli.spawn)(flypath, _ => _),
-    flypath: flypath
-  }))
-  .forEach(_ => {
-    _.spawn.then((fly) => {
-      t.ok(fly instanceof Fly && fly.host && Array.isArray(fly.plugins),
-        "spawn a fly instance using " + _.flypath)
-      t.ok(fly.plugins.length === 1 && fly.fakePlugin,
-        "load plugins from package.json")
+test('✈  cli.help', function (t) {
+	tlog.call(t, cli.help, function (actual) {
+		t.equal(actual, '\n\x1b[2m\x1b[1mUsage\x1b[0m\n  fly [options] [tasks]\n\n\x1b[2m\x1b[1mOptions\x1b[0m\n  \x1b[2m\x1b[1m-\x1b[0m\x1b[1mh \x1b[0m\x1b[2m\x1b[1m --\x1b[0m\x1b[1mhelp\x1b[0m     Display this help.\n  \x1b[2m\x1b[1m-\x1b[0m\x1b[1mf \x1b[0m\x1b[2m\x1b[1m --\x1b[0m\x1b[1mfile\x1b[0m     Use an alternate Flyfile.\n  \x1b[2m\x1b[1m-\x1b[0m\x1b[1ml \x1b[0m\x1b[2m\x1b[1m --\x1b[0m\x1b[1mlist\x1b[0m     Display available tasks.\n  \x1b[2m\x1b[1m-\x1b[0m\x1b[1mv \x1b[0m\x1b[2m\x1b[1m --\x1b[0m\x1b[1mversion\x1b[0m  Display version.\n  ',
+			'show fly help');
+	});
+	t.end();
+});
 
-        fly.filter = (f) => t.equal("ylf", f.cb("fly"),
-          "add plugin function to fly instance and get expected result")
-        fly.fakePlugin()
-    }).catch((e) => t.ok(false, e))
-  })
-})
+test('✈  cli.list', function (t) {
+	var message = 'list tasks in a fly instance';
+	[true, false].forEach(function (bare) {
+		tlog.call(t, function () {
+			cli.list(require(flypath), {bare: bare});
+		}, function (actual, key) {
+			if (!bare && actual === '\n\x1b[2m\x1b[1mAvailable tasks\x1b[0m') {
+				return true;
+			}
+			switch (key) {
+				case 'a':
+					return true;
+				case 'b':
+					return true;
+				case 'c':
+					t.ok(true, 'message / bare ' + bare);
+					break;
+				default:
+					t.ok(false, message);
+			}
+		});
+	});
+	t.end();
+});
+
+test('✈  cli.spawn', function (t) {
+	t.plan(6);
+	[
+		join('test', 'fixtures', 'alt'),
+		join('test', 'fixtures', 'alt', 'flyfile.js')
+		// join("test", "fixtures", "alt", "flyfile.js")
+	].map(function (fpath) {
+		return ({
+			flypath: fpath,
+			spawn: co.wrap(cli.spawn)(fpath, function (_) {
+				console.log('in spawn cb', _);
+				return _;
+			})
+		});
+	}).forEach(function (_) {
+		console.log('im in foreach', _);
+		_.spawn.then(function (fly) {
+			console.log('this is fly', fly);
+			t.ok(fly instanceof Fly && fly.host && Array.isArray(fly.plugins),
+				'spawn a fly instance using ' + _.flypath);
+
+			t.ok(fly.plugins.length === 1 && fly.fakePlugin,
+				'load plugins from package.json');
+
+			fly.filter = function (f) {
+				t.equal('ylf', f.cb('fly'),
+					'add plugin function to fly instance and get expected result');
+			};
+
+			fly.fakePlugin();
+		}).catch(function (e) {
+			t.ok(false, e);
+		});
+	});
+});
