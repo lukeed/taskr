349645d93f98986ce036d20456ad0403ba98115c
luke <luke.edwards05@gmail.com>
2016-02-24 09:38:36 -0800

    update flyfile to reflect current status

--- flyfile.js
+++ flyfile.js
@@ -1,17 +1,17 @@
 var x = module.exports;
 var paths = {
-	src: 'src/**/*.js',
+	src: 'lib/**/*.js',
 	dist: 'dist'
 };
 
 x.default = function * () {
 	/** @desc The default task. */
-	yield this.source(paths.src).xo();
+	// yield this.source(paths.src).xo();
 	yield this.clear(paths.dist);
 	yield this.log('Building Fly...')
 		.source(paths.src)
-		.babel({
-			presets: ['es2015', 'stage-0']
-		})
+		// .babel({
+		// 	presets: ['es2015', 'stage-0']
+		// })
 		.target(paths.dist);
 };
