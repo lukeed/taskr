8ffed4d36ae32f9cdee3588fec368d808f3ab5fb
luke <luke.edwards05@gmail.com>
2016-02-24 17:48:21 -0800

    install & add fly-xo to dev's flyfile.js

--- flyfile.js
+++ flyfile.js
@@ -5,13 +5,11 @@ var paths = {
 };
 
 x.default = function * () {
-	/** @desc The default task. */
-	// yield this.source(paths.src).xo();
+	/** @desc Fly's default development task. */
+	yield this.source(paths.src).xo();
 	yield this.clear(paths.dist);
-	yield this.log('Building Fly...')
+	yield this
+		.log('Building Fly...')
 		.source(paths.src)
-		// .babel({
-		// 	presets: ['es2015', 'stage-0']
-		// })
 		.target(paths.dist);
 };
