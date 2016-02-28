b398f56174fb0f73da8c56204d81ab118ea17ad6
lukee <luke.edwards05@gmail.com>
2016-02-27 18:15:17 -0800

    all tests passing!

--- test/fly.js
+++ test/fly.js
@@ -1,307 +1,349 @@
-import fs from "fs"
-import co from "co"
-import { readFile as read } from "mz/fs"
-import { test } from "tape"
-import Fly from "../src/fly"
-import { join } from "path"
-import touch from "touch"
+var fs = require('fs');
+var co = require('co');
+// var { readFile as read } = require('mz/fs');
+var test = require('tape').test;
+var touch = require('touch');
+var Fly = require('../lib/fly');
+var join = require('path').join;
 
-test("✈  fly", (t) => {
-  t.ok(Fly !== undefined, "is defined")
-  Array.prototype.concat([
-    "source",
-    "filter",
-    "watch",
-    "unwrap",
-    "exec",
-    "start",
-    "write",
-    "clear",
-    "concat",
-    "target",
-    "emit"
-  ]).forEach((method) => t.ok(method !== undefined,
-    method + " is defined"))
-  t.end()
-})
+var fixtures = join(process.cwd(), 'test', 'fixtures');
+var flyfile = join(fixtures, 'flyfile.js');
 
-test("✈  fly.constructor", (t) => {
-  const fly = new Fly({
-    file: join(__dirname, "fixtures", "flyfile.js"),
-    host: {
-      task() {
-        t.equal(fly, this, "bind tasks to fly instance")
-      }
-    }
-  })
-  t.ok(fly !== undefined, "create fly instance")
-  t.ok(fly.tasks.task !== undefined, "load task from host")
-  fly.tasks.task()
-  t.deepEqual(fly.plugins, [], "no default plugins")
-  t.equal(process.cwd(), join(__dirname, "fixtures"),
-    "switch current working directory")
-  t.equal(fly.file, join(__dirname, "fixtures", "flyfile.js"),
-    "set file to path specified in the constructor")
-  t.end()
-})
+test('✈  fly', function (t) {
+	t.ok(Fly !== undefined, 'is defined');
 
-test("✈  fly.source", (t) => {
-  const fly = new Fly()
-  fly.source([[[[["*.a", ["*.b"]]], ["*.c"]]]])
-  t.deepEqual(fly._.globs, ["*.a", "*.b", "*.c"], "flatten globs")
-  t.deepEqual(fly._.filters, [], "init empty filter list")
-  t.end()
-})
+	var methods = [
+		'source', 'filter', 'watch', 'unwrap', 'exec',
+		'start', 'write', 'clear', 'concat', 'target', 'emit'
+	];
 
-test("✈  fly.filter", (t) => {
-  const fly = new Fly()
-  fly.filter((src) => src.toLowerCase())
+	methods.forEach(function (method) {
+		t.ok(method !== undefined, method + ' is defined');
+	});
 
-  t.equal(fly._.filters.length, 1, "add filter to filter collection")
-  t.equal(fly._.filters[0].cb("FLY"), "fly",
-    "add transform cb for anonymous filters")
+	t.end();
+});
 
-  fly.filter("myFilter", (data) => {
-    return {
-      code: data.toString().toUpperCase(),
-      ext: ".foo"
-    }
-  })
+test('✈  fly.constructor', function (t) {
+	var fly = new Fly({
+		file: flyfile,
+		host: {
+			task: function () {
+				t.equal(fly, this, 'bind tasks to fly instance');
+			}
+		}
+	});
 
-  t.ok(fly.myFilter instanceof Function,
-    "add transform cb to fly instance for named filters")
+	t.ok(fly !== undefined, 'create fly instance');
 
-  fly.myFilter({ secret: 42 })
-  t.equal(fly._.filters[1].cb("fly").code, "FLY",
-    "create transform cb function for named filter")
-  t.equal(fly._.filters[1].cb("fly").ext, ".foo", "read extension from filter")
-  t.equal(fly._.filters[1].options.secret, 42, "read options from filter")
+	t.ok(fly.tasks.task !== undefined, 'load task from host');
+	fly.tasks.task();
 
-  try { fly.filter("myFilter")
-  } catch (e) { t.ok(true, "throw an error if filter already exists") }
+	t.deepEqual(fly.plugins, [], 'no default plugins');
 
-  fly.source("")
-  t.deepEqual(fly._.filters, [], "reset filter each time source is called")
+	t.equal(process.cwd(), fixtures, 'switch current working directory');
 
-  t.end()
-})
+	t.equal(fly.file, flyfile, 'set file to path specified in the constructor');
 
-test("✈  fly.watch", (t) => {
-  t.plan(6)
-  const glob = "flyfile.js"
-  const path = join(__dirname, "fixtures", "flyfile.js")
-  const fly = new Fly({
-    file: path,
-    host: {
-      *default(data) {
-        t.ok(true, "run tasks at least once")
-        t.equal(data, 42, "pass options into task via start")
-      }
-    }
-  })
-  fly.emit = (event) => {
-    if (event === "fly_watch") t.ok(true, "notify watch event to observers")
-    return fly
-  }
-  fly.watch(glob, "default", { value: 42 }).then((watcher) => {
-    t.ok(watcher.unwatch !== undefined, "watch promise resolves to a watcher")
-    setTimeout(() =>{
-    // hijack the task to test the watcher runs default when the glob changes
-      fly.host.default = function* (data) {
-        watcher.unwatch(glob)
-        t.ok(true, "run given tasks when glob changes")
-        t.equal(data, 42, "pass options into task via start on change")
-      }
-      touch(path)
-    }, 100)
-  })
-})
+	t.end();
+});
 
-test("✈  fly.unwrap", (t) => {
-  t.plan(4)
-  const files_x = ["a.x", "b.x", "c.x"]
-  const files_y = ["a.y", "b.y", "c.y"]
-  const files = files_x.concat(files_y)
-  const paths = files.map((file) => {
-    const path = join(__dirname, "fixtures", file)
-    touch(path)
-    return path
-  })
-  const fly = new Fly()
-  co(function* () {
-    yield fly.source("*.x").unwrap((f) => {
-      t.deepEqual(f, files_x, "unwrap source globs with single entry point")
-    })
-    yield fly.source(["*.y"]).unwrap((f) => {
-      t.deepEqual(f, files_y, "unwrap source globs with single entry point in array")
-    })
-    const result = yield fly.source(["*.x", "*.y"]).unwrap((f) => {
-      t.deepEqual(f, files, "unwrap source globs with multiple entry points")
-      return 42
-    })
-    t.equal(result, 42, "result is the return value from fulfilled handler")
-    yield fly.clear(files)
-  })
-})
+test('✈  fly.source', function (t) {
+	var fly = new Fly();
+	fly.source([[[[['*.a', ['*.b']]], ['*.c']]]]);
+	t.deepEqual(fly._.globs, ['*.a', '*.b', '*.c'], 'flatten globs');
+	t.deepEqual(fly._.filters, [], 'init empty filter list');
+	t.end();
+});
 
-test("✈  fly.*exec", (t) => {
-  t.plan(4)
-  const fly = new Fly({
-    host: {
-      *task(data) {
-        t.ok(true, "run a task")
-        t.equal(data, "rosebud", "pass an initial value to task")
-      }
-    }
-  })
-  fly.emit = (event, options) => {
-    if (event === "task_start")
-      t.ok(true, "notify start event to observers")
-    if (event === "task_complete")
-      t.ok(true, "notify complete event to observers")
-    return fly
-  }
-  co(fly.exec.bind(fly), "task", "rosebud")
-})
+test('✈  fly.filter', function (t) {
+	var fly = new Fly();
 
-test("✈  fly.start", (t) => {
-  t.plan(3)
-  const value = 42
-  const fly = new Fly({
-    host: {
-      *a(data) {
-        return data + 1
-      },
-      *b(data) {
-        return data + 1
-      },
-      *c(data) {
-        t.ok(true, "run a given list of tasks")
-        t.equal(data, value + 2, "cascade return values")
-        return data + 1
-      }
-    }
-  })
-  co(function* () {
-    const result = yield fly.start(["a", "b", "c"], { value: value })
-    t.equal(result, value + 3, "return last task value")
-  })
-})
+	fly.filter(function (src) {
+		return src.toLowerCase();
+	});
 
-test("✈  fly.start (order)", (t, state) => {
-  t.plan(2)
-  state = 0
-  const fly = new Fly({
-    host: {
-    // when running in a sequence both b and c wait while a blocks.
-    // when running in parallel, b and c run while a blocks. state
-    // can only be 3 when each task runs in order.
-      *a() {
-        yield block()
-        if (state === 0) state++
-      },
-      *b() { state++ },
-      *c() { state++ }
-    }
-  })
-  co(function* () {
-    yield fly.start(["a", "b", "c"], { parallel: false })
-    t.ok(state === 3, "run tasks in sequence")
-    state = 0 // reset
-    yield fly.start(["a", "b", "c"], { parallel: true })
-    t.ok(state !== 3, "run tasks in parallel")
-  })
-  function block () {
-    return new Promise((resolve) => setTimeout(() => resolve(), 200))
-  }
-})
+	t.equal(fly._.filters.length, 1, 'add filter to filter collection');
 
-test("✈  fly.clear", (t) => {
-  t.plan(1)
-  const paths = Array.prototype.concat(["tmp1", "tmp2", "tmp3"])
-  .map((file) => {
-    const path = join(__dirname, "fixtures", file)
-    touch(path)
-    return path
-  })
-  const fly = new Fly()
-  co(function* () {
-    yield fly.clear(paths)
-    t.ok(true, "clear files from a given list of paths")
-  })
-})
+	t.equal(fly._.filters[0].cb('FLY'), 'fly', 'add transform cb for anonymous filters');
 
-test("✈  fly.concat", (t) => {
-  const fly = new Fly()
-  fly.concat("f")
-  t.equal(fly._.cat.base, "f", "add concat writer")
-  t.end()
-})
+	fly.filter('myFilter', function (data) {
+		return {
+			code: data.toString().toUpperCase(),
+			ext: '.foo'
+		};
+	});
 
-test("✈  fly.flatten", (t) => {
-  t.plan(4)
-  const fly = new Fly()
-  const src = join(__dirname, "fixtures", "**/*.md")
-  const dest = join(__dirname, "fixtures", "dest")
+	t.ok(fly.myFilter instanceof Function, 'add transform cb to fly instance for named filters');
 
-  const results_norm = ["one"]
-  const results_zero = ["one.md", "two-1.md", "two-2.md"]
-  const results_one = ["one", "two"]
+	fly.myFilter({secret: 42});
 
-  function* matches (dir, expect) {
-    const data = fs.readdirSync(dir)
-    return (expect.length == data.length) && expect.every((u, i) => u === data[i])
-  }
+	t.equal(fly._.filters[1].cb('fly').code, 'FLY', 'create transform cb function for named filter');
+	t.equal(fly._.filters[1].cb('fly').ext, '.foo', 'read extension from filter');
+	t.equal(fly._.filters[1].options.secret, 42, 'read options from filter');
 
-  co(function* () {
-    const tar = `${dest}-1`
+	try {
+		fly.filter('myFilter');
+	} catch (e) {
+		t.ok(true, 'throw an error if filter already exists');
+	}
 
-    yield fly.source(src).target(tar)
-    t.ok(yield matches(tar, results_norm), "retain normal pathing if desired depth not specified")
-    yield fly.clear(tar)
-  })
+	fly.source('');
+	t.deepEqual(fly._.filters, [], 'reset filter each time source is called');
 
-  co(function* () {
-    const tar = `${dest}-2`
+	t.end();
+});
 
-    yield fly.source(src).target(tar, {depth: 0})
-    t.ok(yield matches(tar, results_zero), "move all files to same directory, no parents")
-    yield fly.clear(tar)
-  })
+// test('✈  fly.watch', function (t) {
+// 	t.plan(6);
+// 	var glob = 'flyfile.js';
+// 	var file = flyfile;
 
-  co(function* () {
-    const tar = `${dest}-3`
+// 	var fly = new Fly({
+// 		file: file,
+// 		host: {
+// 			default: function (data) {
+// 				t.ok(true, 'run tasks at least once');
+// 				t.equal(data, 42, 'pass options into task via start');
+// 			}
+// 		}
+// 	});
 
-    yield fly.source(src).target(tar, {depth: 1})
-    t.ok(yield matches(tar, results_one), "keep one parent directory per file")
-    yield fly.clear(tar)
-  })
+// 	fly.emit = function (event) {
+// 		if (event === 'fly_watch') {
+// 			t.ok(true, 'notify watch event to observers');
+// 		}
+// 		return fly;
+// 	};
 
-  co(function* () {
-    const tar = `${dest}-4`
+// 	console.log('this is glob', glob);
 
-    yield fly.source(src).target(tar, {depth: 5})
-    t.ok(yield matches(tar, results_norm), "retain full path if desired depth exceeds path depth")
-    yield fly.clear(tar)
-  })
-})
+// 	fly.watch(glob, 'default', {value: 42}).then(function (watcher) {
+// 		console.log('this is watcher', watcher);
+// 		t.ok(watcher.unwatch !== undefined, 'watch promise resolves to a watcher');
+// 		setTimeout(function () {
+// 			// hijack the task to test the watcher runs default when the glob changes
+// 			fly.host.default = function * (data) {
+// 				watcher.unwatch(glob);
+// 				t.ok(true, 'run given tasks when glob changes');
+// 				t.equal(data, 42, 'pass options into task via start on change');
+// 			};
+// 			touch(file);
+// 		}, 100);
+// 	});
+// });
 
-test("✈  fly.target", (t) => {
-  t.plan(1)
-  co(function* () {
-    process.chdir(join(__dirname, "fixtures"))
-    const fly = new Fly()
-    yield fly
-      .source("*.txt")
-      .filter((data) => data.toString().toUpperCase())
-      .target(".")
+test('✈  fly.unwrap', function (t) {
+	t.plan(4);
+	var fly = new Fly();
 
-    yield fly
-      .source("*.txt")
-      .filter((data) => {
-        t.ok(data.toString() === "FOO BAR\n", "resolve source, filters and writers")
-        return data.toString().toLowerCase()
-      })
-      .target(".")
-  })
-})
+	var xFiles = ['a.x', 'b.x', 'c.x'];
+	var yFiles = ['a.y', 'b.y', 'c.y'];
+	var files = xFiles.concat(yFiles);
+
+	files.map(function (file) {
+		var f = join(fixtures, file);
+		touch(f);
+		return f;
+	});
+
+	co(function * () {
+		yield fly.source('*.x').unwrap(function (f) {
+			t.deepEqual(f, xFiles, 'unwrap source globs with single entry point');
+		});
+
+		yield fly.source(['*.y']).unwrap(function (f) {
+			t.deepEqual(f, yFiles, 'unwrap source globs with single entry point in array');
+		});
+
+		var result = yield fly.source(['*.x', '*.y']).unwrap(function (f) {
+			t.deepEqual(f, files, 'unwrap source globs with multiple entry points');
+			return 42;
+		});
+
+		t.equal(result, 42, 'result is the return value from fulfilled handler');
+
+		yield fly.clear(files);
+	});
+});
+
+test('✈  fly.*exec', function (t) {
+	t.plan(4);
+
+	var fly = new Fly({
+		host: {
+			task: function * (data) {
+				t.ok(true, 'run a task');
+				t.equal(data, 'rosebud', 'pass an initial value to task');
+			}
+		}
+	});
+
+	fly.emit = function (event) {
+		if (event === 'task_start') {
+			t.ok(true, 'notify start event to observers');
+		}
+		if (event === 'task_complete') {
+			t.ok(true, 'notify complete event to observers');
+		}
+		return fly;
+	};
+
+	co(fly.exec.bind(fly), 'task', 'rosebud');
+});
+
+test('✈  fly.start', function (t) {
+	t.plan(3);
+	var value = 42;
+	var fly = new Fly({
+		host: {
+			a: function * (data) {
+				return data + 1;
+			},
+			b: function * (data) {
+				return data + 1;
+			},
+			c: function * (data) {
+				t.ok(true, 'run a given list of tasks');
+				t.equal(data, value + 2, 'cascade return values');
+				return data + 1;
+			}
+		}
+	});
+
+	co(function * () {
+		var result = yield fly.start(['a', 'b', 'c'], {value: value});
+		t.equal(result, value + 3, 'return last task value');
+	});
+});
+
+test('✈  fly.start (order)', function (t, state) {
+	t.plan(2);
+	state = 0;
+	var fly = new Fly({
+		// when running in a sequence both b and c wait while a blocks.
+		// when running in parallel, b and c run while a blocks. state
+		// can only be 3 when each task runs in order.
+		host: {
+			a: function * () {
+				yield block();
+				if (state === 0) {
+					state++;
+				}
+			},
+			b: function * () {
+				state++;
+			},
+			c: function * () {
+				state++;
+			}
+		}
+	});
+
+	co(function * () {
+		yield fly.start(['a', 'b', 'c'], {parallel: false});
+		t.ok(state === 3, 'run tasks in sequence');
+		state = 0; // reset
+		yield fly.start(['a', 'b', 'c'], {parallel: true});
+		t.ok(state !== 3, 'run tasks in parallel');
+	});
+
+	function block() {
+		return new Promise(function (resolve) {
+			return setTimeout(resolve, 200);
+		});
+	}
+});
+
+test('✈  fly.clear', function (t) {
+	t.plan(1);
+	var paths = ['tmp1', 'tmp2', 'tmp3'].map(function (file) {
+		var f = join(fixtures, file);
+		touch(f);
+		return f;
+	});
+
+	var fly = new Fly();
+
+	co(function * () {
+		yield fly.clear(paths);
+		t.ok(true, 'clear files from a given list of paths');
+	});
+});
+
+test('✈  fly.concat', function (t) {
+	var fly = new Fly();
+	fly.concat('f');
+	t.equal(fly._.cat.base, 'f', 'add concat writer');
+	t.end();
+});
+
+test('✈  fly.flatten', function (t) {
+	t.plan(4);
+	var fly = new Fly();
+
+	var src = join(fixtures, '**/*.md');
+	var dest = join(fixtures, 'dest');
+
+	var resultsNormal = ['one'];
+	var resultsZero = ['one.md', 'two-1.md', 'two-2.md'];
+	var resultsOne = ['one', 'two'];
+
+	function * matches(dir, expect) {
+		var data = fs.readdirSync(dir);
+		return (expect.length === data.length) && expect.every(function (u, i) {
+			return u === data[i];
+		});
+	}
+
+	co(function * () {
+		var tar = dest + '-1';
+
+		yield fly.source(src).target(tar);
+		t.ok(yield matches(tar, resultsNormal), 'retain normal pathing if desired depth not specified');
+		yield fly.clear(tar);
+	});
+
+	co(function * () {
+		var tar = dest + '-2';
+
+		yield fly.source(src).target(tar, {depth: 0});
+		t.ok(yield matches(tar, resultsZero), 'move all files to same directory, no parents');
+		yield fly.clear(tar);
+	});
+
+	co(function * () {
+		var tar = dest + '-3';
+
+		yield fly.source(src).target(tar, {depth: 1});
+		t.ok(yield matches(tar, resultsOne), 'keep one parent directory per file');
+		yield fly.clear(tar);
+	});
+
+	co(function* () {
+		var tar = dest + '-4';
+
+		yield fly.source(src).target(tar, {depth: 5});
+		t.ok(yield matches(tar, resultsNormal), 'retain full path if desired depth exceeds path depth');
+		yield fly.clear(tar);
+	});
+});
+
+test('✈  fly.target', function (t) {
+	t.plan(1);
+
+	co(function * () {
+		process.chdir(fixtures);
+
+		var fly = new Fly();
+
+		yield fly.source('*.txt').filter(function (data) {
+			return data.toString().toUpperCase();
+		}).target('.');
+
+		yield fly.source('*.txt').filter(function (data) {
+			t.ok(data.toString() === 'FOO BAR\n', 'resolve source, filters and writers');
+			return data.toString().toLowerCase();
+		}).target('.');
+	});
+});
