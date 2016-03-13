7bd543c0c78601dda71b10190a41d42187d9dd34
luke <luke.edwards05@gmail.com>
2016-03-13 15:27:26 -0700

    tests: no semicolons. fix arrays

--- test/emitter.js
+++ test/emitter.js
@@ -1,22 +1,22 @@
-var util = require('util');
-var test = require('tape').test;
-var Emitter = require('../lib/emitter');
+var util = require('util')
+var test = require('tape').test
+var Emitter = require('../lib/emitter')
 
 function Subscriber() {
-	Emitter.call(this);
+	Emitter.call(this)
 }
-util.inherits(Subscriber, Emitter);
+util.inherits(Subscriber, Emitter)
 
 test('✈  Emitter', function (t) {
-	t.ok(Emitter !== undefined, 'is defined');
+	t.ok(Emitter !== undefined, 'is defined')
 
-	var e = new Subscriber();
-	t.deepEqual(e.events, [], 'inherit events collection');
+	var e = new Subscriber()
+	t.deepEqual(e.events, [], 'inherit events collection')
 
 	e.on('my_event', function (data) {
-		t.deepEqual(data, {data: 1}, 'notify events');
-		t.end();
-	});
+		t.deepEqual(data, {data: 1}, 'notify events')
+		t.end()
+	})
 
-	e.emit('my_event', {data: 1});
-});
+	e.emit('my_event', {data: 1})
+})
