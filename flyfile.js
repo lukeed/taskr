7ff936e32b8f4cf4c3ed32b4b3c5b69418f4f27e
luke <luke.edwards05@gmail.com>
2016-03-13 14:39:08 -0700

    no semicolons inside lib/

--- flyfile.js
+++ flyfile.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-var src = 'lib/**/*.js';
+var src = 'lib/**/*.js'
 
 module.exports.default = function * () {
 	/** @desc Fly's default development task. */
-	yield this.source(src).xo();
+	yield this.source(src).xo()
 	yield this
 		.log('Building Fly...')
-		.source(src).target('tmp');
-	yield this.clear('tmp');
-};
+		.source(src).target('tmp')
+	yield this.clear('tmp')
+}
