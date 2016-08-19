dac2241f34b72d1243ffc5a3d3f00aa0c00cca27
Luke Edwards <luke.edwards05@gmail.com>
2016-08-19 01:27:47 -0700

    fix emitter test

--- test/emitter.js
+++ test/emitter.js
@@ -1,22 +1,19 @@
-var util = require('util')
-var test = require('tape').test
-var Emitter = require('../lib/emitter')
+const util = require('util');
+const test = require('tape').test;
+const Emitter = require('../lib/emitter');
 
-function Subscriber() {
-	Emitter.call(this)
-}
-util.inherits(Subscriber, Emitter)
+class Subscriber extends Emitter {}
 
-test('✈  Emitter', function (t) {
-	t.ok(Emitter !== undefined, 'is defined')
+test('✈  Emitter', t => {
+	t.ok(Emitter !== undefined, 'is defined');
 
-	var e = new Subscriber()
-	t.deepEqual(e.events, [], 'inherit events collection')
+	const e = new Subscriber();
+	t.deepEqual(e.events, [], 'inherit events collection');
 
-	e.on('my_event', function (data) {
-		t.deepEqual(data, {data: 1}, 'notify events')
-		t.end()
-	})
+	e.on('my_event', data => {
+		t.deepEqual(data, {data: 1}, 'notify events');
+		t.end();
+	});
 
-	e.emit('my_event', {data: 1})
-})
+	e.emit('my_event', {data: 1});
+});
