69624a08125dc857ac82213e525a8acd7de8ba47
Luke Edwards <luke.edwards05@gmail.com>
2016-08-21 11:24:44 -0700

    rewrite utils' tests

--- test/utils.js
+++ test/utils.js
@@ -1,101 +1,96 @@
-var fs = require('fs')
-var path = require('path')
-var test = require('tape').test
-var utils = require('../lib/utils')
-var join = path.join
+const fs = require('fs');
+const path = require('path');
+const test = require('tape').test;
+const utils = require('../lib/utils');
+const join = path.join;
 
-var fixtures = join(process.cwd(), 'test', 'fixtures', 'utils')
+const fixtures = join(process.cwd(), 'test', 'fixtures', 'utils');
 
 function asyncFunc(value, handler) {
-	setTimeout(function () {
-		return (handler(undefined, value))
-	}, 100)
+	setTimeout(() => handler(undefined, value), 100);
 }
 
 function asyncFuncWithOptions(value, options, handler) {
-	setTimeout(function () {
-		return (handler(undefined, value))
-	}, options.time)
+	setTimeout(() => handler(undefined, value), options.time);
 }
 
-test('fly utilities ✈', function (t) {
-	t.ok(utils !== undefined, 'it\'s real')
+test('fly utilities ✈', t => {
+	t.ok(utils !== undefined, 'it\'s real');
 
-	var methods = ['bind', 'defer', 'find', 'log', 'error', 'alert', 'stamp', 'trace']
+	['defer', 'find', 'log', 'error', 'alert', 'stamp', 'trace']
+		.forEach(prop => {
+			t.ok(utils[prop] !== undefined, `${prop} is defined`);
+		});
 
-	methods.forEach(function (prop) {
-		t.ok(utils[prop] !== undefined, prop + ' is defined')
-	})
+	t.end();
+});
 
-	t.end()
-})
+test('utils.defer (asyncFunc) ✈', t => {
+	t.plan(1);
+	utils.defer(asyncFunc)(42).then(value => {
+		t.equal(value, 42, 'promisifies an async func');
+	});
+});
 
-test('utils.defer (asyncFunc) ✈', function (t) {
-	t.plan(1)
-	utils.defer(asyncFunc)(42).then(function (value) {
-		t.equal(value, 42, 'promisifies an async func')
-	})
-})
+test('utils.defer (asyncFunc w/ options) ✈', t => {
+	t.plan(1);
+	utils.defer(asyncFuncWithOptions)(1985, {time: 100}).then(value => {
+		t.equal(value, 1985, 'promisifies an async func w/ options');
+	});
+});
 
-test('utils.defer (asyncFunc /w options) ✈', function (t) {
-	t.plan(1)
-	utils.defer(asyncFuncWithOptions)(1985, {time: 100}).then(function (value) {
-		t.equal(value, 1985, 'promisifies an async func w/ options')
-	})
-})
+test('utils.find (flyfile) ✈', t => {
+	t.plan(4);
 
-test('utils.find (flyfile) ✈', function (t) {
-	t.plan(4)
+	const name = 'flyfile.js';
+	const full = join(fixtures, name);
 
-	var name = 'flyfile.js'
-	var full = join(fixtures, name)
+	utils.find(name, fixtures).then(fp => {
+		t.ok(fp !== undefined, 'finds a flyfile, given a directory');
+		t.equal(fp, full, 'finds the right one!');
+	});
 
-	utils.find(name, fixtures).then(function (fp) {
-		t.ok(fp !== undefined, 'finds a flyfile, given a directory')
-		t.equal(fp, full, 'finds the right one!')
-	})
+	utils.find(full).then(fp => {
+		t.equal(fp, full, 'finds a flyfile, given a filepath');
+	});
 
-	utils.find(full).then(function (fp) {
-		t.equal(fp, full, 'finds a flyfile, given a filepath')
-	})
+	const dir = join(fixtures, 'one'); // test dir
+	utils.find(name, dir).then(fp => {
+		t.equal(fp, full, 'finds a flyfile, traversing upwards');
+	});
+});
 
-	var dir = join(fixtures, 'one') // test dir
-	utils.find(name, dir).then(function (fp) {
-		t.equal(fp, full, 'finds a flyfile, traversing upwards')
-	})
-})
+test('utils.read (file)', t => {
+	const fp = join(fixtures, 'a.js');
+	utils.read(fp).then(data => {
+		t.equal(data.toString(), 'const pi = 3.14\n', 'reads file contents');
+		t.end();
+	});
+});
 
-test('utils.read (file)', function (t) {
-	var fp = join(fixtures, 'a.js')
-	utils.read(fp).then(function (data) {
-		t.equal(data.toString(), 'const pi = 3.14\n', 'reads a file\'s contents')
-		t.end()
-	})
-})
+test('utils.read (dir)', t => {
+	utils.read(fixtures).then(data => {
+		t.true(data === null, 'will not attempt to read a directory');
+		t.end();
+	});
+});
 
-test('utils.read (dir)', function (t) {
-	utils.read(fixtures).then(function (data) {
-		t.true(data === null, 'will not attempt to read a directory')
-		t.end()
-	})
-})
+test('utils.write', t => {
+	const fp = join(fixtures, 'test.js');
+	const data = 'hello';
 
-test('utils.write', function (t) {
-	var fp = join(fixtures, 'test.js')
-	var data = 'hello'
+	utils.write(fp, data).then(() => {
+		return utils.find(fp).then(f => {
+			t.true(f !== undefined, 'file was created');
 
-	utils.write(fp, data).then(function () {
-		return utils.find(fp).then(function (f) {
-			t.true(f !== undefined, 'file was created')
-
-			return utils.read(fp).then(function (d) {
-				t.deepEqual(d.toString(), data, 'file had data')
+			return utils.read(fp).then(d => {
+				t.deepEqual(d.toString(), data, 'file had data');
 
 				// delete test file
-				fs.unlinkSync(fp)
+				fs.unlinkSync(fp);
 
-				t.end()
-			})
-		})
-	})
-})
+				t.end();
+			});
+		});
+	});
+});
