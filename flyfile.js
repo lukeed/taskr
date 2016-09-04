46bb36ac06c8d93c88296b994c53de23f8b6b7d8
Luke Edwards <luke.edwards05@gmail.com>
2016-09-03 20:33:07 -0700

    add note within serial error catch

--- flyfile.js
+++ flyfile.js
@@ -2,7 +2,7 @@
 
 var src = 'lib/**/*.js'
 
-module.exports.default = function * () {
+exports.default = function * () {
 	/** @desc Fly's default development task. */
 	yield this.source(src).xo()
 	yield this
