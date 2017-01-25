a2184f9d3254cba921dae25b67b3621e5281df3b
Luke Edwards <luke.edwards05@gmail.com>
2017-01-25 11:28:55 -0800

    re-enable fly.plugins' parameter testing - now that inline plugins can be accepted

--- test/plugins.js
+++ test/plugins.js
@@ -124,73 +124,73 @@ test("fly.plugins", co(function* (t) {
 	t.end()
 }))
 
-// test("fly.plugins' parameters", co(function* (t) {
-// 	t.plan(15)
+test("fly.plugins' parameters", co(function* (t) {
+	t.plan(15)
 
-// 	const ext = "*.txt"
-// 	const src = join(fixtures, ext)
-// 	const tar = join(fixtures, ".tmp")
+	const ext = "*.txt"
+	const src = join(fixtures, ext)
+	const tar = join(fixtures, ".tmp")
+	const expect = ["p0", "p1", "p2", "p3", "p4", "p5"]
 
-// 	const fly = new Fly({
-// 		plugins: {
-// 			func() {
-// 				this.plugin("p0", {}, function* (one) {
-// 					// x2 bcuz 2 files
-// 					t.true($.isObject(one), "1st param is a `file` object (`every: 1`)")
-// 				})
-// 			}
-// 		}, {
-// 			func() {
-// 				this.plugin("p1", { every: 0 }, function* (one, two, thr) {
-// 					t.true(Array.isArray(one), "1st param is a `files` array (`every: 0`)")
-// 					t.deepEqual(two, {}, "2nd param defaults to empty object")
-// 					t.equal(thr, undefined, "3rd param defaults as undefined")
-// 				})
-// 			}
-// 		}, {
-// 			func() {
-// 				this.plugin("p2", { every: 0 }, function* (_, two, thr) {
-// 					t.equal(two, "hi", "2nd param can be a `string`")
-// 					t.equal(thr, undefined, "3rd param remains undefined")
-// 				})
-// 			}
-// 		}, {
-// 			func() {
-// 				this.plugin("p3", { every: 0 }, function* (_, two, thr) {
-// 					t.deepEqual(two, ["hi"], "2nd param can be an `array`")
-// 					t.equal(thr, undefined, "3rd param remains undefined")
-// 				})
-// 			}
-// 		}, {
-// 			func() {
-// 				this.plugin("p4", { every: 0 }, function* (_, two, thr) {
-// 					t.deepEqual(two, { a: "hi" }, "2nd param can be a custom `object`")
-// 					t.equal(thr, undefined, "3rd param remains undefined")
-// 				})
-// 			}
-// 		}, {
-// 			func() {
-// 				this.plugin("p5", { every: 0 }, function* (_, two, thr) {
-// 					t.deepEqual(thr, { a: "hi" }, "3rd param can be assigned")
-// 					t.equal(two, "hello", "2nd param also assigned")
-// 				})
-// 			}
-// 		}],
-// 		tasks: {
-// 			* a(f) {
-// 				yield f.source(src).p0().target(tar)
-// 				yield f.source(src).p1().target(tar)
-// 				yield f.source(src).p2("hi").target(tar)
-// 				yield f.source(src).p3(["hi"]).target(tar)
-// 				yield f.source(src).p4({ a: "hi" }).target(tar)
-// 				yield f.source(src).p5("hello", { a: "hi" }).target(tar)
-// 				yield del(tar)
-// 			}
-// 		}
-// 	})
+	const fly = new Fly({
+		plugins: [{
+			name: 'p0',
+			*func(one) {
+				// x2 bcuz 2 files
+				t.true($.isObject(one), "1st param is a `file` object (`every: 1`)")
+			}
+		}, {
+			every: 0,
+			name: 'p1',
+			*func(one, two, thr) {
+				t.true(Array.isArray(one), "1st param is a `files` array (`every: 0`)")
+				t.deepEqual(two, {}, "2nd param defaults to empty object")
+				t.equal(thr, undefined, "3rd param defaults as undefined")
+			}
+		}, {
+			every: 0,
+			name: 'p2',
+			*func(_, two, thr) {
+				t.equal(two, "hi", "2nd param can be a `string`")
+				t.equal(thr, undefined, "3rd param remains undefined")
+			}
+		}, {
+			every: 0,
+			name: 'p3',
+			*func(_, two, thr) {
+				t.deepEqual(two, ["hi"], "2nd param can be an `array`")
+				t.equal(thr, undefined, "3rd param remains undefined")
+			}
+		}, {
+			every: 0,
+			name: 'p4',
+			*func(_, two, thr) {
+				t.deepEqual(two, { a: "hi" }, "2nd param can be a custom `object`")
+				t.equal(thr, undefined, "3rd param remains undefined")
+			}
+		}, {
+			every: 0,
+			name: 'p5',
+			*func(_, two, thr) {
+				t.deepEqual(thr, { a: "hi" }, "3rd param can be assigned")
+				t.equal(two, "hello", "2nd param also assigned")
+			}
+		}],
+		tasks: {
+			* a(f) {
+				yield f.source(src).p0().target(tar)
+				yield f.source(src).p1().target(tar)
+				yield f.source(src).p2("hi").target(tar)
+				yield f.source(src).p3(["hi"]).target(tar)
+				yield f.source(src).p4({ a: "hi" }).target(tar)
+				yield f.source(src).p5("hello", { a: "hi" }).target(tar)
+				yield del(tar)
+			}
+		}
+	})
 
-// 	t.true("p0" in fly, "attach first custom plugin (`p0`) to fly")
-// 	t.true("p5" in fly, "attach first custom plugin (`p0`) to fly")
+	t.deepEqual(fly.plugNames, expect, "store all custom plugin names")
+	t.equal(Object.keys(fly.plugins).length, expect.length, "attach all plugin methods")
 
-// 	yield fly.start("a")
-// }))
+	yield fly.start("a")
+}))
