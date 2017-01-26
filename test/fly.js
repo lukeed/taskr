3d944415b9eb7e678fe4b9916884a82695b63cb4
Luke Edwards <luke.edwards05@gmail.com>
2017-01-26 00:23:16 -0800

    add ALL Task tests! (class & runtime)

--- test/fly.js
+++ test/fly.js
@@ -74,70 +74,6 @@ test("fly.constructor (exits)", t => {
 	t.end()
 })
 
-// test("fly.init", co(function* (t) {
-// 	const RDY = "_ready"
-// 	const fly1 = new Fly({
-// 		tasks: {
-// 			* a(f) {
-// 				t.equal(f, fly1, "pass fly instance to tasks")
-// 			}
-// 		}
-// 	})
-
-// 	t.false(fly1[RDY], "is not ready before `fly.init`")
-
-// 	fly1.init()
-
-// 	t.true(fly1[RDY], "is ready after `fly.init`")
-
-// 	fly1.tasks.a()
-
-// 	const fly2 = new Fly({ tasks: [1, 2, 3] })
-// 	fly2.on("tasks_force_object", () => {
-// 		t.pass("emit an error if `tasks` is not an object")
-// 	})
-// 	fly2.init()
-
-// 	t.end()
-// }))
-
-// test("fly.source", co(function* (t) {
-// 	t.plan(18)
-
-// 	const fly = new Fly()
-// 	const glob1 = ["*.a", "*.b", "*.c"]
-// 	const glob2 = join(fixtures, "*.*")
-// 	const opts1 = { ignore: "foo" }
-
-// 	fly.on("globs_no_match", (g, o) => {
-// 		t.pass("notify when globs match no files")
-// 		t.deepEqual(g, glob1, "warning receives the flattened globs")
-// 		t.deepEqual(o, opts1, "warning receives the `expand` options")
-// 	})
-
-// 	const out = yield fly.source([[["*.a", ["*.b"]]], ["*.c"]], opts1)
-// 	t.true("globs" in fly._ && "files" in fly._, "create `globs` and `files` keys within `fly._`")
-// 	t.deepEqual(fly._.globs, glob1, "flatten nested globs")
-// 	t.deepEqual(fly._.files, [], "return empty array if no files matched")
-// 	t.equal(out, fly, "returns the bound instance")
-
-// 	yield fly.source(glob2)
-// 	t.true(Array.isArray(fly._.globs), "wrap a single glob string as an array")
-// 	t.equal(fly._.globs[0], glob2, "update internal `source` keys each time")
-// 	t.true(Array.isArray(fly._.files), "return an array of relevant files")
-// 	t.equal(fly._.files.length, 4, "accepts wildcard extensions finds all files")
-// 	const f1 = fly._.files[0]
-// 	t.ok($.isObject(f1), "array contents are objects")
-// 	t.ok("data" in f1, "add `data` key to `pathObject`")
-// 	t.false("root" in f1, "delete `root` key from `pathObject`")
-// 	t.false("name" in f1, "delete `name` key from `pathObject`")
-// 	t.false("ext" in f1, "delete `ext` key from `pathObject`")
-// 	t.ok(Buffer.isBuffer(f1.data), "file data is a `Buffer`")
-
-// 	yield fly.source(glob2, { ignore: join(fixtures, "flyfile.js") })
-// 	t.equal(fly._.files.length, 3, "send config options to `expand` (ignore key)")
-// }))
-
 test("fly.start", co(function* (t) {
 	let val
 
@@ -176,8 +112,7 @@ test("fly.start", co(function* (t) {
 		}
 	}
 
-	yield fly2.start("a")
-	yield fly2.start("b")
+	yield fly2.serial(["a", "b"])
 	try {
 		yield fly2.start("err")
 	} catch (e) {
@@ -337,113 +272,6 @@ test("fly.serial", co(function* (t) {
 	yield fly2.serial(["a", "b", "c"])
 	t.equal(num, 2, "interrupt `serial` on error only 2 tasks ran")
 }))
-
-// test("fly.run", co(function* (t) {
-// 	t.plan(9)
-// 	const src = join(fixtures, "*.txt")
-// 	const tar = join(fixtures, ".tmp")
-
-// 	const fly = new Fly({
-// 		tasks: {
-// 			* a(f, o) {
-// 				const t = o.val
-// 				yield f.source(src).run({}, function* (file) {
-// 					t.true($.isObject(file), "iterate thru each `file` by default")
-// 					t.true("data" in file, "entire `file` object is accessed")
-// 					file.data = new Buffer(file.data.toString().toUpperCase())
-// 				}).target(tar)
-// 			},
-// 			* b(f, o) {
-// 				const t = o.val
-// 				yield f.source(src).run({ every: 0 }, function* (files) {
-// 					t.true(Array.isArray(files), "allow inline `run` to use `every: 0`")
-// 				}).target(tar)
-// 			}
-// 		}
-// 	})
-
-// 	yield fly.start("a", { val: t })
-// 	const arr1 = yield fly.$.expand(`${tar}/*.txt`)
-// 	t.equal(arr1.length, 2, "place files in target destination after `inline` method")
-// 	const str1 = yield fly.$.read(`${tar}/foo.txt`, "utf8")
-// 	const str2 = yield fly.$.read(`${tar}/bar.txt`, "utf8")
-// 	t.equal(str1, "FOO BAR\n", "capitalize file contents individually")
-// 	t.equal(str2, "BAR BAZ\n", "capitalize file contents individually")
-// 	yield del(tar)
-
-// 	yield fly.start("b", { val: t })
-// 	const arr2 = yield fly.$.expand(`${tar}/*.txt`)
-// 	t.equal(arr2.length, 2, "place files in target destination after `inline` method")
-// 	yield del(tar)
-// }))
-
-// test("fly.target", co(function* (t) {
-// 	const glob1 = join(fixtures, "one", "two", "*.md")
-// 	const glob2 = join(fixtures, "one", "*.md")
-// 	const glob3 = join(fixtures, "**", "*.md")
-// 	const glob4 = join(fixtures, "one", "**", "*.md")
-
-// 	const dest1 = join(fixtures, ".tmp1")
-// 	const dest2 = join(fixtures, ".tmp2")
-// 	const dest3 = join(fixtures, ".tmp3")
-// 	const dest4 = join(fixtures, ".tmp4")
-// 	const dest5 = join(fixtures, ".tmp5")
-// 	const dest6 = join(fixtures, ".tmp6")
-
-// 	const fly = new Fly({
-// 		plugins: [{
-// 			name: "fakeConcat",
-// 			func() {
-// 				this.plugin("fakeConcat", { every: 0 }, function* (all) {
-// 					this._.files = [{ dir: all[0].dir, base: "fake.foo", data: new Buffer("bar") }]
-// 				})
-// 			}
-// 		}]
-// 	})
-
-// 	// clean slate
-// 	yield del([dest1, dest2, dest3, dest4, dest5])
-
-// 	// test #1
-// 	yield fly.source(glob1).target(dest1)
-// 	t.pass("allow method chains!")
-// 	const val1 = join(dest1, "two", "two-1.md")
-// 	const str1 = yield fly.$.find(val1)
-// 	const arr1 = yield fly.$.expand(join(dest1, "*.md"))
-// 	t.equal(arr1.length, 2, "via `src/one/two/*.md` write all files")
-// 	t.ok(str1 && str1 !== val1, "via `src/one/two/*.md` did not create sub-dir if unwanted")
-
-// 	// test #2
-// 	yield fly.source(glob2).target(dest2)
-// 	const arr2 = yield fly.$.expand(join(dest2, "*.md"))
-// 	const str2 = yield fly.$.find(join(dest2, "one.md"))
-// 	t.equal(arr2.length, 1, "via `src/one/*.md` write all files")
-// 	t.ok(str2.length, "via `src/one/*.md` write to correct tier")
-
-// 	// test #3
-// 	yield fly.source(glob3).target(dest3)
-// 	const arr3 = yield fly.$.expand(join(dest3, "**", "*.md"))
-// 	const str3 = yield fly.$.find(join(dest3, "one", "two", "two-1.md"))
-// 	t.ok(str3.length, "via `src/**/*.md` create the (nested) child directory")
-// 	t.equal(arr3.length, 3, "via `src/**/*.md` write all files")
-
-// 	// test #4
-// 	yield fly.source(glob4).target([dest4, dest5])
-// 	const str4 = yield fly.$.find(join(dest4, "two", "two-1.md"))
-// 	const str5 = yield fly.$.find(join(dest5, "two", "two-1.md"))
-// 	t.true(str4.length && str5.length, "write to multiple targets")
-
-// 	yield fly.source(glob4).target(dest6).fakeConcat().target(`${dest6}/sub`)
-// 	t.pass("allow `target()` to be chained")
-// 	const str6 = yield fly.$.find(join(dest6, "two", "two-1.md"))
-// 	const str7 = yield fly.$.find(join(dest6, "sub", "fake.foo"))
-// 	t.true(str6.length && str7.length, "perform all actions in double-target chain")
-
-// 	// clean up
-// 	yield del([dest1, dest2, dest3, dest4, dest5, dest6])
-
-// 	t.end()
-// }))
 
 test("fly.plugin (conflict)", co(function * (t) {
 	t.plan(6)
---
+++ test/task.js
@@ -0,0 +1,290 @@
+"use strict"
+
+const Promise = require("bluebird")
+const join = require("path").join
+const test = require("tape")
+
+const Task = require("../lib/task")
+const Fly = require("../lib/fly")
+const del = require("./helpers")
+const $ = require("../lib/fn")
+
+const protos = ["source", "target", "start", "parallel", "serial", "emit", "run", "exec"]
+const fixtures = join(__dirname, "fixtures")
+const co = Promise.coroutine
+
+test("Task.constructor (basic)", co(function * (t) {
+	const fly = new Fly()
+	const task = new Task(fly)
+	t.true($.isObject(task), "returns an object")
+	protos.forEach(str => {
+		t.true(task.hasOwnProperty(str), `Task.prototype.${str} is defined`)
+	})
+	t.end()
+}))
+
+test("Task.constructor (plugins)", co(function * (t) {
+	const fly = new Fly({
+		tasks: { *a(){} },
+		plugins: [{ name: "foo", *func(){} }]
+	})
+	const task = new Task(fly)
+	t.true(task.hasOwnProperty("foo"), "inherits the `foo()` plugin")
+	t.equal(typeof task.foo, "function", "mounts correctly as a function")
+	t.end()
+}))
+
+test("task.constructor (internal)", co(function * (t) {
+	t.plan(27)
+
+	const fly = new Fly({
+		tasks: {
+			*foo(f) {
+				t.true(f instanceof Task, "task receives bound Task instance")
+				protos.forEach(str => t.true(str in f, `Task.prototype.${str} is defined`))
+				t.true(f.hasOwnProperty("_") && $.isObject(f._), "task contains internal `_` object")
+				t.true(f.hasOwnProperty("$") && $.isObject(f.$), "task accesses all `$` helpers")
+				Array("files", "globs", "prevs").forEach(str => {
+					t.true(f._.hasOwnProperty(str), `task._.${str} is defined`)
+					t.true(Array.isArray(f._[str]), `task._.${str} is an array`)
+					t.equal(f._[str].length, 0, `task._.${str} is empty`)
+				})
+			},
+			*bar(f, o) {
+				t.true(o !== undefined, "task receives a 2nd parameter")
+				t.true($.isObject(o), "task's config is an object by default")
+				Array("val", "src").forEach(el => {
+					t.true(o.hasOwnProperty(el), `task config object has \`${el}\` key`)
+					t.equal(o[el], null, `task \`opts.${el}\` is \`null\` by default`)
+				})
+			},
+			*baz(f, o) {
+				t.equal(o.val, 5, "task config can be customized (opts.val)")
+			}
+		}
+	})
+
+	yield fly.parallel(["foo", "bar"]).start("baz", {val: 5})
+}))
+
+test("task.source", co(function * (t) {
+	t.plan(18)
+
+	const glob1 = ["*.a", "*.b", "*.c"]
+	const glob2 = join(fixtures, "*.*")
+	const opts1 = { ignore: "foo" }
+
+	const fly = new Fly({
+		tasks: {
+			*foo(f) {
+				yield f.source([[["*.a", ["*.b"]]], ["*.c"]], opts1)
+				t.deepEqual(f._.globs, glob1, "flattens deeply nested globs")
+				t.deepEqual(f._.files, [], "returns empty array when no files matched")
+			},
+			*bar(f) {
+				yield f.source(glob2)
+				t.true(Array.isArray(f._.globs), "converts a single glob to an array")
+				t.equal(f._.globs[0], glob2, "updates internal source knowledge")
+				t.true(Array.isArray(f._.files), "returns array of matching files")
+				t.equal(f._.files.length, 4, "accepts wildcard extensions to find all files")
+
+				const f1 = f._.files[0]
+				t.true($.isObject(f1), "array members are file objects")
+				t.true(f1.hasOwnProperty("data"), "adds `data` key to `pathObject`")
+				t.true(f1.hasOwnProperty("base"), "keeps `base` key from `pathObject`")
+				t.true(f1.hasOwnProperty("dir"), "keeps `dir` key from `pathObject`")
+				t.false(f1.hasOwnProperty("root"), "deletes `root` key from `pathObject`")
+				t.false(f1.hasOwnProperty("name"), "deletes `name` key from `pathObject`")
+				t.false(f1.hasOwnProperty("ext"), "deletes `ext` key from `pathObject`")
+				t.ok(Buffer.isBuffer(f1.data), "file's `data` is a `Buffer`")
+			},
+			*baz(f) {
+				yield f.source(glob2, { ignore: join(fixtures, "flyfile.js") })
+				t.equal(f._.files.length, 3, "tunnels options to `utils.expand` (ignore)")
+			}
+		}
+	})
+
+	fly.on("globs_no_match", (g, o) => {
+		t.pass("notify when globs match no files")
+		t.deepEqual(g, glob1, "warning receives the flattened globs")
+		t.deepEqual(o, opts1, "warning receives the `expand` options")
+	})
+
+	yield fly.parallel(["foo", "bar", "baz"])
+}))
+
+test("task.target", co(function * (t) {
+	t.plan(10)
+	const glob1 = join(fixtures, "one", "two", "*.md")
+	const glob2 = join(fixtures, "one", "*.md")
+	const glob3 = join(fixtures, "**", "*.md")
+	const glob4 = join(fixtures, "one", "**", "*.md")
+
+	const dest1 = join(fixtures, ".tmp1")
+	const dest2 = join(fixtures, ".tmp2")
+	const dest3 = join(fixtures, ".tmp3")
+	const dest4 = join(fixtures, ".tmp4")
+	const dest5 = join(fixtures, ".tmp5")
+	const dest6 = join(fixtures, ".tmp6")
+
+	// clean slate
+	yield del([dest1, dest2, dest3, dest4, dest5])
+
+	const fly = new Fly({
+		plugins: [{
+			every: 0,
+			name: "fakeConcat",
+			*func(all) {
+				this._.files = [{ dir: all[0].dir, base: "fake.foo", data: new Buffer("bar") }]
+			}
+		}],
+		tasks: {
+			*a(f) {
+				yield f.source(glob1).target(dest1)
+				t.pass("allow method chains!")
+
+				const g = join(dest1, "*.md")
+				const v = join(dest1, "two", "two-1.md")
+
+				const str = yield f.$.find(v)
+				const arr = yield f.$.expand(g)
+				t.equal(arr.length, 2, "via `src/one/two/*.md` write all files")
+				t.ok(str && str !== v, "via `src/one/two/*.md` did not create sub-dir if unwanted")
+			},
+			*b(f) {
+				yield f.source(glob2).target(dest2)
+				const arr = yield f.$.expand(join(dest2, "*.md"))
+				const str = yield f.$.find(join(dest2, "one.md"))
+				t.equal(arr.length, 1, "via `src/one/*.md` write all files")
+				t.ok(str.length, "via `src/one/*.md` write to correct tier")
+			},
+			*c(f) {
+				yield f.source(glob3).target(dest3)
+				const arr = yield f.$.expand(join(dest3, "**", "*.md"))
+				const str = yield f.$.find(join(dest3, "one", "two", "two-1.md"))
+				t.ok(str.length, "via `src/**/*.md` create the (nested) child directory")
+				t.equal(arr.length, 3, "via `src/**/*.md` write all files")
+			},
+			*d(f) {
+				yield f.source(glob4).target([dest4, dest5])
+				const str1 = yield f.$.find(join(dest4, "two", "two-1.md"))
+				const str2 = yield f.$.find(join(dest5, "two", "two-1.md"))
+				t.true(str1.length && str2.length, "write to multiple targets")
+			},
+			*e(f) {
+				yield f.source(glob4).target(dest6).fakeConcat().target(`${dest6}/sub`)
+				t.pass("allow `target()` to be chained")
+				const str1 = yield f.$.find(join(dest6, "two", "two-1.md"))
+				const str2 = yield f.$.find(join(dest6, "sub", "fake.foo"))
+				t.true(str1.length && str2.length, "perform all actions in double-target chain")
+			}
+		}
+	})
+
+	yield fly.parallel(["a", "b", "c", "d", "e"])
+	// clean up
+	yield del([dest1, dest2, dest3, dest4, dest5, dest6])
+}))
+
+test("task.run (w/function)", co(function * (t) {
+	t.plan(10)
+	const src = join(fixtures, "*.txt")
+	const tar1 = join(fixtures, ".tmp1")
+	const tar2 = join(fixtures, ".tmp2")
+
+	const fly = new Fly({
+		tasks: {
+			*foo(f) {
+				yield f.source(src).run({}, function * (file) {
+					t.true($.isObject(file), "iterates thru each `file` (default)")
+					t.true(file.hasOwnProperty("data"), "receives entire `file` object")
+					file.data = new Buffer(file.data.toString().toUpperCase())
+					yield Promise.resolve()
+				}).target(tar1)
+
+				const arr = yield f.$.expand(`${tar1}/*.txt`)
+				t.equal(arr.length, 2, "place files in target destination after `inline` method (every:1)")
+				const str1 = yield f.$.read(`${tar1}/foo.txt`, "utf8")
+				const str2 = yield f.$.read(`${tar1}/bar.txt`, "utf8")
+				t.equal(str1, "FOO BAR\n", "capitalize file contents individually")
+				t.equal(str2, "BAR BAZ\n", "capitalize file contents individually")
+			},
+			*bar(f) {
+				yield f.source(src).run({ every: 0 }, function * (files) {
+					t.true(Array.isArray(files), "customizes inline plugin behavior")
+					t.equal(files.length, 2, "receives array of ALL current files (every:0)")
+				}).target(tar2)
+
+				const arr = yield f.$.expand(`${tar2}/*.txt`)
+				t.equal(arr.length, 2, "place files in target destination after `inline` method (every:0)")
+			}
+		}
+	})
+
+	yield fly.parallel(["foo", "bar"])
+	yield del([tar1, tar2])
+}))
+
+test("task.run (w/object)", co(function * (t) {
+	t.plan(10)
+	const src = join(fixtures, "*.txt")
+	const tar1 = join(fixtures, ".tmp1")
+	const tar2 = join(fixtures, ".tmp2")
+
+	const fly = new Fly({
+		tasks: {
+			*foo(f) {
+				yield f.source(src).run({
+					*func(file) {
+						t.true($.isObject(file), "iterates thru each `file` (default)")
+						t.true(file.hasOwnProperty("data"), "receives entire `file` object")
+						file.data = new Buffer(file.data.toString().toUpperCase())
+						yield Promise.resolve()
+					}
+				}).target(tar1)
+
+				const arr = yield f.$.expand(`${tar1}/*.txt`)
+				t.equal(arr.length, 2, "place files in target destination after `inline` method (every:1)")
+				const str1 = yield f.$.read(`${tar1}/foo.txt`, "utf8")
+				const str2 = yield f.$.read(`${tar1}/bar.txt`, "utf8")
+				t.equal(str1, "FOO BAR\n", "capitalize file contents individually")
+				t.equal(str2, "BAR BAZ\n", "capitalize file contents individually")
+			},
+			*bar(f) {
+				yield f.source(src).run({
+					every: 0,
+					*func(files) {
+						t.true(Array.isArray(files), "customizes inline plugin behavior")
+						t.equal(files.length, 2, "receives array of ALL current files (every:0)")
+					}
+				}).target(tar2)
+
+				const arr = yield f.$.expand(`${tar2}/*.txt`)
+				t.equal(arr.length, 2, "place files in target destination after `inline` method (every:0)")
+			}
+		}
+	})
+
+	yield fly.parallel(["foo", "bar"])
+	yield del([tar1, tar2])
+}))
+
+test("task.exec", co(function * (t) {
+	t.plan(4)
+	const fly = new Fly()
+	const task = new Task(fly)
+
+	const obj1 = { foo: "bar" }
+	const obj2 = { baz: "bat" }
+	// mock task function, pre-wrapped
+	const mock = co(function * (f, o) {
+		t.true(f instanceof Task, "receives current Task instance as parameter")
+		t.deepEqual(f._, obj2, "sets internal `task._` object during exchange")
+		t.true(this instanceof Task, "binds `this` context to Task instance")
+		t.deepEqual(o, obj1, "receives custom set of options")
+		return this
+	})
+
+	yield task.exec(mock, obj1, obj2)
+}))
