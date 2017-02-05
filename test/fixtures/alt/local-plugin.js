48e9ab8ce598b8bed3af40f15e2ec2fc9c78cbca
Luke Edwards <luke.edwards05@gmail.com>
2017-02-05 10:16:20 -0800

    Continue Node4 (LTS) Support (#258)
    * revert to node4 minimum
    
    * remove spreads & destructured assignments

--- test/fixtures/alt/local-plugin.js
+++ test/fixtures/alt/local-plugin.js
@@ -2,7 +2,8 @@
 
 module.exports = function (fly, utils) {
 	const self = this
-	fly.plugin("localPlugin", {every: 0}, function * (_, { t }) {
+	fly.plugin("localPlugin", {every: 0}, function * (_, opts) {
+		const t = opts.t
 		t.true("root" in fly && "emit" in fly && "tasks" in fly, "plugin creator receives `fly` instance")
 		t.true("expand" in utils && "find" in utils && "write" in utils, "plugin creator receives `utils` helpers object")
 		t.deepEqual(fly, self, "plugin creator context bound to `fly` instance")
