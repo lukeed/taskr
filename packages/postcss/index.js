9182da7ff1d90d3fb4c07b0b1f58b153e531e702
Luke Edwards <luke.edwards05@gmail.com>
2017-06-20 11:10:57 -0700

    extract setError helper

--- packages/postcss/index.js
+++ packages/postcss/index.js
@@ -4,6 +4,8 @@ const extn = require('path').extname;
 const postcss = require('postcss');
 
 module.exports = function (task) {
+	const setError = msg => task.emit('plugin_error', { plugin:'@taskr/postcss', error:msg });
+
 	task.plugin('postcss', {}, function * (file, opts) {
 		opts = Object.assign({ plugins:[], options:{} }, opts);
 
@@ -11,11 +13,8 @@ module.exports = function (task) {
 			const ctx = postcss(opts.plugins);
 			const out = yield ctx.process(file.data.toString(), opts);
 			file.data = Buffer.from(out.css); // write new data
-		} catch (error) {
-			task.emit('plugin_error', {
-				plugin: '@taskr/postcss',
-				error: error.message
-			});
+		} catch (err) {
+			return setError(err.message);
 		}
 	})
 }
