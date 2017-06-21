a466be4aef697932ac8fe3181344a61532b956c9
Luke Edwards <luke.edwards05@gmail.com>
2017-06-20 22:30:04 -0700

    refactor config/options handling
    - forgot that `opts` param defaults to empty object
    - imported taskr’s ‘isEmptyObj’ fn helper

--- packages/postcss/index.js
+++ packages/postcss/index.js
@@ -5,7 +5,7 @@ const postcss = require('postcss');
 
 const base = { plugins:[], options:{} };
 const isObject = any => Boolean(any) && (any.constructor === Object);
-const isNullType = any => any == null;
+const isEmptyObj = any => isObject(any) && Object.keys(any).length === 0;
 
 module.exports = function (task, utils) {
 	const rootPath = str => res(task.root, str);
@@ -16,24 +16,45 @@ module.exports = function (task, utils) {
 	task.plugin('postcss', { every:false }, function * (files, opts) {
 		let config;
 
-		if (isObject(opts)) {
-			config = opts;
-		} else if (isNullType(opts)) {
+		if (isEmptyObj(opts)) {
+			console.log('WAS NULL OPTS', task.root);
 			// look for `.postcssrc` or "postcss" key in `package.json`
-			config = JSON.parse(yield readRoot('.postcssrc')) || JSON.parse(yield readRoot('package.json')).postcss;
-			if (isNullType(config)) {
+			config = JSON.parse(yield readRoot('.postcssrc')) || JSON.parse(yield readRoot('package.json') || '{}').postcss;
+			// these files will only be JSON objects (only)
+			if (isObject(config)) {
+				// reconstruct "plugins"
+				let fn, key, plugins = [];
+				const old = config.plugins || {};
+				for (key in old) {
+					console.log(`this is key!!! ${key}`);
+					try {
+						console.log(key, old[key]);
+						fn = require(key)(old[key] || {});
+						plugins.push(fn);
+					} catch (err) {
+						console.log('errored', err);
+						return setError(`Loading PostCSS plugin (${key}) failed with: ${err.message}`);
+					}
+				}
+				// update config
+				config.plugins = plugins;
+			} else {
 				// look for `postcss.config.js` or `.postcssrc.js`
 				const fileConfig = (yield findRoot('postcss.config.js')) || (yield findRoot('.postcssrc.js'));
-				if (!isNullType(fileConfig)) {
+				if (fileConfig !== null) {
 					// pass thru `require` because always `module.exports`
 					config = require(fileConfig);
 					// handle function exports, pass default value
 					(typeof config === 'function') && (config = config(base));
 				}
 			}
+		} else {
+			config = opts; // wasnt empty
 		}
 
 		config = config || {};
+
+		console.log('FINAL CONFIG', config);
 
 		if (!isObject(config)) {
 			return setError(`Invalid PostCSS config! An object is required; recevied: ${typeof config}`);
