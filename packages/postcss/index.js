e8987385e53e22b4a8bfe317c43955d2eb5f9cba
Luke Edwards <luke.edwards05@gmail.com>
2017-06-21 00:17:28 -0700

    second refactor; cleaner
    - look once for a file, then decide what to do based on result

--- packages/postcss/index.js
+++ packages/postcss/index.js
@@ -4,57 +4,58 @@ const res = require('path').resolve;
 const postcss = require('postcss');
 
 const base = { plugins:[], options:{} };
+const filenames = ['.postcssrc', '.postcssrc.js', 'postcss.config.js', 'package.json'];
+
 const isObject = any => Boolean(any) && (any.constructor === Object);
 const isEmptyObj = any => isObject(any) && Object.keys(any).length === 0;
 
 module.exports = function (task, utils) {
-	const rootPath = str => res(task.root, str);
-	const findRoot = str => utils.find(rootPath(str));
-	const readRoot = str => utils.read(rootPath(str), 'utf8');
+	const rootDir = str => res(task.root, str);
 	const setError = msg => task.emit('plugin_error', { plugin:'@taskr/postcss', error:msg });
+	const getConfig = arr => Promise.all(arr.map(utils.find)).then(res => res.filter(Boolean)).then(res => res[0]);
 
 	task.plugin('postcss', { every:false }, function * (files, opts) {
 		let config;
 
 		if (isEmptyObj(opts)) {
-			console.log('WAS NULL OPTS', task.root);
-			// look for `.postcssrc` or "postcss" key in `package.json`
-			config = JSON.parse(yield readRoot('.postcssrc')) || JSON.parse(yield readRoot('package.json') || '{}').postcss;
-			// these files will only be JSON objects (only)
-			if (isObject(config)) {
-				// reconstruct "plugins"
-				let fn, key, plugins = [];
-				const old = config.plugins || {};
-				for (key in old) {
-					console.log(`this is key!!! ${key}`);
+			// autoload a file
+			const fileConfig = yield getConfig(filenames.map(rootDir));
+			// process if found one!
+			if (fileConfig !== void 0) {
+				try {
+					config = require(fileConfig);
+				} catch (err) {
 					try {
-						console.log(key, old[key]);
-						fn = require(key)(old[key] || {});
-						plugins.push(fn);
-					} catch (err) {
-						console.log('errored', err);
-						return setError(`Loading PostCSS plugin (${key}) failed with: ${err.message}`);
+						config = JSON.parse(yield utils.read(fileConfig, 'utf8')); // .rc file
+					} catch (_) {
+						return setError(err.message);
 					}
 				}
-				// update config
-				config.plugins = plugins;
-			} else {
-				// look for `postcss.config.js` or `.postcssrc.js`
-				const fileConfig = (yield findRoot('postcss.config.js')) || (yield findRoot('.postcssrc.js'));
-				if (fileConfig !== null) {
-					// pass thru `require` because always `module.exports`
-					config = require(fileConfig);
-					// handle function exports, pass default value
-					(typeof config === 'function') && (config = config(base));
+				// handle config types
+				if (typeof config === 'function') {
+					config = config(base); // send default values
+				} else if (isObject(config)) {
+					// grab "postcss" key (package.json)
+					config = config.postcss || config;
+					// reconstruct plugins?
+					if (isObject(config.plugins)) {
+						let k, plugins = [];
+						const old = config.plugins || {};
+						for (k in old) {
+							try {
+								plugins.push(require(k)(old[k]));
+							} catch (err) {
+								return setError(`Loading PostCSS plugin (${k}) failed with: ${err.message}`);
+							}
+						}
+						// update config
+						config.plugins = plugins;
+					}
 				}
 			}
-		} else {
-			config = opts; // wasnt empty
 		}
 
-		config = config || {};
-
-		console.log('FINAL CONFIG', config);
+		config = config || opts;
 
 		if (!isObject(config)) {
 			return setError(`Invalid PostCSS config! An object is required; recevied: ${typeof config}`);
