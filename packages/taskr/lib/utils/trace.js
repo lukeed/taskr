271fb7d8511b76d514c07309cdc2c0644360618a
Luke Edwards <luke.edwards05@gmail.com>
2017-06-29 00:03:59 -0700

    consolidate utils.trace

--- packages/taskr/lib/utils/trace.js
+++ packages/taskr/lib/utils/trace.js
@@ -1,20 +1,11 @@
-"use strict"
+'use strict';
 
-const extractPath = /\s+at.*(?:\(|\s)(.*)\)?/
-const isPath = /^(?:(?:(?:node|(?:internal\/[\w/]*)?\w+)\.js:\d+:\d+)|native)/
+const extractPath = /\s+at.*(?:\(|\s)(.*)\)?/;
+const isPath = /^(?:(?:(?:node|(?:internal\/[\w/]*)?\w+)\.js:\d+:\d+)|native)/;
 
 module.exports = function (stack) {
-	return stack.replace(/\\/g, "/")
-		.split("\n")
-		.filter(x => {
-			const pathMatches = x.match(extractPath)
-
-			if (pathMatches === null || !pathMatches[1]) {
-				return true;
-			}
-
-			return !isPath.test(pathMatches[1])
-		})
-		.filter(x => x.trim() !== "")
-		.join("\n")
+	return stack.replace(/\\/g, '/').split('\n').filter(x => {
+		const matches = x.match(extractPath);
+		return matches === null || !matches[1] || !isPath.test(matches[1]);
+	}).filter(x => x.trim() !== '').join('\n');
 }
