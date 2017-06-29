654a136a23e9acd56f2d2eb7c60fef2fea6426d9
Luke Edwards <luke.edwards05@gmail.com>
2017-06-28 23:02:18 -0700

    replace `mkdirp` with `mk-dirs`
    - removes minimist@0.0.8 as sub-dep
    - roughly 15% faster

--- packages/taskr/lib/utils/write.js
+++ packages/taskr/lib/utils/write.js
@@ -1,23 +1,21 @@
-"use strict"
+'use strict'
 
-const p = require("path")
-const Promise = require("bluebird")
-const write = Promise.promisify(require("fs").writeFile)
-const mkdirp = Promise.promisify(require("mkdirp"))
-const normalize = p.normalize
-const dirname = p.dirname
+const p = require('path')
+const mkdir = require('mk-dirs')
+const Promise = require('bluebird')
+const write = Promise.promisify(require('fs').writeFile)
 
 /**
  * Write to a file with given data.
  * Creates ancestor directories if needed.
- * @param {String} file  The full file"s path.
+ * @param {String} file  The full file's path.
  * @param {String} data  The data to write.
  * @param {Object} opts  See `fs.writeFile`.
  */
 module.exports = Promise.coroutine(function * (file, data, opts) {
 	try {
-		file = normalize(file)
-		yield mkdirp(dirname(file))
-		yield write(file, data, opts)
+		file = p.normalize(file);
+		yield mkdir(p.dirname(file));
+		yield write(file, data, opts);
 	} catch (_) {}
-})
+});
