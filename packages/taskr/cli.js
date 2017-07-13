49a69ef6008025a116e5dc362f9bdeec5be3e627
Luke Edwards <luke.edwards05@gmail.com>
2017-07-12 23:47:28 -0700

    fix cli code style

--- packages/taskr/cli.js
+++ packages/taskr/cli.js
@@ -1,46 +1,46 @@
 #!/usr/bin/env node
-"use strict"
+'use strict';
 
-const Promise = require("bluebird")
-const reporter = require("./lib/reporter")
-const utils = require("./lib/utils")
-const cli = require("./lib/cli")
-const pkg = require("./package")
-const co = Promise.coroutine
+const Promise = require('bluebird');
+const reporter = require('./lib/reporter');
+const utils = require('./lib/utils');
+const cli = require('./lib/cli');
+const pkg = require('./package');
+const co = Promise.coroutine;
 
 co(function* () {
 	// get command options
-	const o = cli.options()
-	const t = o.tasks.length ? o.tasks : ["default"]
+	const o = cli.options();
+	const t = o.tasks.length ? o.tasks : ['default'];
 
 	if (o.help) {
-		return cli.help()
+		return cli.help();
 	}
 
 	if (o.version) {
-		return cli.version(pkg)
+		return cli.version(pkg);
 	}
 
-	const taskr = yield cli.spawn(o.cwd)
-	reporter.call(taskr)
+	const taskr = yield cli.spawn(o.cwd);
+	reporter.call(taskr);
 
 	if (!taskr.file) {
-		return taskr.emit("taskfile_not_found")
+		return taskr.emit('taskfile_not_found');
 	}
 
 	if (o.list) {
-		return cli.list(taskr.tasks, o.list === "bare")
+		return cli.list(taskr.tasks, o.list === 'bare');
 	}
 
 	// announce start
-	taskr.emit("task_run", taskr.file)
+	taskr.emit('task_run', taskr.file);
 	// run `tasks` in `mode`
-	taskr[o.mode](t)
+	taskr[o.mode](t);
 
 })().catch(err => {
-	if (err.type === "cli") {
-		utils.error(`CLI Error! ${err.message}`)
+	if (err.type === 'cli') {
+		utils.error(`CLI Error! ${err.message}`);
 	} else {
-		console.error(utils.trace(err.stack))
+		console.error(utils.trace(err.stack));
 	}
-})
+});
